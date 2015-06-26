1e38314d35962430d9d2ab6eb8478dea954baba3
benji6 <benhall22@hotmail.co.uk>
2015-06-26 13:44:35 +0100

    not noteController's responsibilty to keep track of currently playing notes

--- client/js/noteController.js
+++ client/js/noteController.js
@@ -1,4 +1,4 @@
-import {propEq, reject} from 'ramda';
+import {propEq} from 'ramda';
 import VirtualAudioGraph from 'virtual-audio-graph';
 import pingPongDelay from './customVirtualNodes/pingPongDelay';
 import oscillatorBank from './customVirtualNodes/oscillatorBank';
@@ -10,15 +10,9 @@ const virtualAudioGraph = new VirtualAudioGraph();
 virtualAudioGraph.defineNode(pingPongDelay, 'pingPongDelay');
 virtualAudioGraph.defineNode(oscillatorBank, 'oscillatorBank');
 
-const currentlyPlayingPitches = new Set();
-
 export const playNote = ({pitch, modulation}) => {
-  if (currentlyPlayingPitches.has(pitch)) {
-    return;
-  }
   modulation = modulation === undefined ? 0.5 : modulation;
 
-  currentlyPlayingPitches.add(pitch);
   virtualAudioGraph.update([
     {
       output: 'output',
@@ -37,13 +31,10 @@ export const playNote = ({pitch, modulation}) => {
   ]);
 };
 
-export const stopNote = ({pitch}) => {
-  currentlyPlayingPitches.delete(pitch);
-  virtualAudioGraph.update([
-    {
-      output: 'output',
-      id: 0,
-      node: 'pingPongDelay',
-    },
-  ]);
-};
+export const stopNote = (note) => virtualAudioGraph.update([
+  {
+    output: 'output',
+    id: 0,
+    node: 'pingPongDelay',
+  },
+]);
