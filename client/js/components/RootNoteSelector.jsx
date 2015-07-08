ade025af394228858a2579b3cadb77787e76084f
benji6 <benhall22@hotmail.co.uk>
2015-07-08 14:40:37 +0100

    display note name instead of pitch

--- client/js/components/RootNoteSelector.jsx
+++ client/js/components/RootNoteSelector.jsx
@@ -4,7 +4,16 @@ import render from '../tools/render';
 import RootNoteStore from '../stores/RootNoteStore';
 import RootNoteActions from '../actions/RootNoteActions';
 
+const {abs, floor} = Math;
+
 let boundOnChange = null;
+
+const computeNoteNameFromPitch = (pitch) => {
+  const AlphabeticalComponents = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+  const numberOfNotes = AlphabeticalComponents.length;
+  const octave = floor(pitch / numberOfNotes) + 4;
+  return AlphabeticalComponents[abs(pitch) % numberOfNotes] + octave;
+};
 
 export default class RootNoteSelector extends React.Component {
   constructor (props) {
@@ -41,7 +50,7 @@ export default class RootNoteSelector extends React.Component {
       <div className="modal-window">
         <div className="modal-contents">
           <h1>Root Note</h1>
-            <output>{this.state.rootNote}</output>
+            <output>{computeNoteNameFromPitch(this.state.rootNote)}</output>
           <div>
             <input max="24" min="-36" type="range" value={this.state.rootNote} onChange={this.handleChange}></input>
           </div>
