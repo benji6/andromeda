854922e00d08d857a765228b389bb0f10d35a074
benji6 <benhall22@hotmail.co.uk>
2015-07-08 14:43:41 +0100

    add harmonic minor scale

--- client/js/stores/ScaleStore.js
+++ client/js/stores/ScaleStore.js
@@ -4,6 +4,7 @@ import ScaleActions from '../actions/ScaleActions';
 export default alt.createStore(class ScaleStore {
   constructor () {
     this.scales = {
+      'harmonic minor': [0, 2, 3, 5, 7, 8, 11, 12],
       major: [0, 2, 4, 5, 7, 9, 11, 12],
       minor: [0, 2, 3, 5, 7, 8, 10, 12],
       pentatonic: [0, 3, 5, 7, 10, 12],
