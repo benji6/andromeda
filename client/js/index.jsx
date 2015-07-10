c8bc1d5096634710960e9dfbf3d7e1f2e443f3d7
benji6 <benhall22@hotmail.co.uk>
2015-07-10 11:20:16 +0100

    added please upgrade your browser view

--- client/js/index.jsx
+++ client/js/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import alt from './alt';
 import PerformanceView from './components/PerformanceView';
+import UpgradeBrowserView from './components/UpgradeBrowserView';
 import keyboard from './keyboard';
 import render from './tools/render';
 
-
-// jshint ignore: start
-render(<PerformanceView />);
-// jshint ignore: end
+if ('serviceWorker' in navigator) {
+  // jshint ignore: start
+  render(<PerformanceView />);
+  // jshint ignore: end
+} else {
+  // jshint ignore: start
+  render(<UpgradeBrowserView />);
+  // jshint ignore: end
+}
