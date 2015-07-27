fa4f73c3866c1e95b3983b1b2ab2e532ee999e93
benji6 <benhall22@hotmail.co.uk>
2015-07-27 22:31:56 +0100

    fix upgrade browser view

--- client/scripts/components/atoms/UpgradeBrowserView.js
+++ client/scripts/components/atoms/UpgradeBrowserView.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class UpgradeBrowserView extends React.Component {
   render () {
-    return <h1 className="loading-placeholder">Requires the latest version of Chrome to run...</h1>;
+    return <h1 className="upgrade-browser-message">Requires the latest version of Chrome to run...</h1>;
   }
 }
