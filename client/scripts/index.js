d4f1841aec8630429fbba288f0eec39dcb91758a
benji6 <benhall22@hotmail.co.uk>
2015-08-04 21:04:21 +0100

    restructure components

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -2,12 +2,12 @@ import './customVirtualNodes/defineNodes';
 import './alt';
 import './stores/ArpeggiatorStore';
 import React from 'react'; // eslint-disable-line no-unused-vars
-import PerformanceView from './components/PerformanceView';
-import UpgradeBrowserView from './components/atoms/UpgradeBrowserView';
+import PerformanceView from './components/pages/PerformanceView';
+import UpgradeBrowserView from './components/pages/UpgradeBrowserView';
 import './keyboard';
 import render from './tools/render';
 
-if ('serviceWorker' in navigator) {
+if (navigator['serviceWorker']) {
   render(<PerformanceView />);
 } else {
   render(<UpgradeBrowserView />);
