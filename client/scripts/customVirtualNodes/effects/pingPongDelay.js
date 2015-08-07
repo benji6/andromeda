80463b6438e0b2c326aafef43f310d0019687c73
benji6 <benhall22@hotmail.co.uk>
2015-08-07 22:40:46 +0100

    more es6y

--- client/scripts/customVirtualNodes/effects/pingPongDelay.js
+++ client/scripts/customVirtualNodes/effects/pingPongDelay.js
@@ -1,54 +1,51 @@
-export default (params = {}) => {
-  let {decay, delayTime, maxDelayTime} = params;
-  decay = decay !== undefined ? decay : 1 / 3;
-  delayTime = delayTime !== undefined ? delayTime : 1 / 3;
-  maxDelayTime = maxDelayTime !== undefined ? maxDelayTime : 1 / 3;
-
-  return {
-    0: {
-      node: 'stereoPanner',
-      output: 'output',
-      params: {
-        pan: -1,
-      },
+export default ({
+  decay = 1 / 3,
+  delayTime = 1 / 3,
+  maxDelayTime = 1 / 3,
+} = {}) => ({
+  zero: {
+    node: 'stereoPanner',
+    output: 'output',
+    params: {
+      pan: -1,
     },
-    1: {
-      node: 'stereoPanner',
-      output: 'output',
-      params: {
-        pan: 1,
-      },
+  },
+  1: {
+    node: 'stereoPanner',
+    output: 'output',
+    params: {
+      pan: 1,
     },
-    2: {
-      node: 'delay',
-      output: [1, 5],
-      params: {
-        maxDelayTime,
-        delayTime,
-      },
+  },
+  2: {
+    node: 'delay',
+    output: [1, 'five'],
+    params: {
+      maxDelayTime: maxDelayTime,
+      delayTime: delayTime,
     },
-    3: {
-      node: 'gain',
-      output: 2,
-      params: {
-        gain: decay,
-      },
+  },
+  3: {
+    node: 'gain',
+    output: 2,
+    params: {
+      gain: decay,
     },
-    4: {
-      node: 'delay',
-      output: [0, 3],
-      params: {
-        maxDelayTime,
-        delayTime,
-      },
+  },
+  4: {
+    node: 'delay',
+    output: ['zero', 3],
+    params: {
+      maxDelayTime: maxDelayTime,
+      delayTime: delayTime,
     },
-    5: {
-      input: 'input',
-      node: 'gain',
-      output: 4,
-      params: {
-        gain: decay,
-      },
+  },
+  five: {
+    input: 'input',
+    node: 'gain',
+    output: 4,
+    params: {
+      gain: decay,
     },
-  };
-};
+  },
+});
