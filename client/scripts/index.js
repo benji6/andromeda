d509d1f391fcad4d537c09e0c709d5c3bd9464e7
benji6 <benhall22@hotmail.co.uk>
2015-09-10 23:10:09 +0100

    switching the control pad back in

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,20 +1,20 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
-import {Provider} from 'react-redux';
+// import {Provider} from 'react-redux';
 import './customVirtualNodes/defineNodes';
-import PatternEditor from './components/templates/PatternEditor';
-// import PerformanceView from './components/templates/PerformanceView';
-// import UpgradeBrowserView from './components/templates/UpgradeBrowserView';
+// import PatternEditor from './components/templates/PatternEditor';
+import PerformanceView from './components/templates/PerformanceView';
+import UpgradeBrowserView from './components/templates/UpgradeBrowserView';
 import './keyboard';
 import render from './tools/render';
-import store from './store';
+// import store from './store';
 
 if (navigator.serviceWorker) {
-  render(
-    <Provider store={store}>
-      {() => <PatternEditor />}
-    </Provider>
-  );
-//   render(<PerformanceView />);
-// } else {
-//   render(<UpgradeBrowserView />);
+  // render(
+  //   <Provider store={store}>
+  //     {() => <PatternEditor />}
+  //   </Provider>
+  // );
+  render(<PerformanceView />);
+} else {
+  render(<UpgradeBrowserView />);
 }
