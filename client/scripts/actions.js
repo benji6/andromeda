008ba9b291f459c0778add60cf26d0f2034465f6
benji6 <benhall22@hotmail.co.uk>
2015-09-10 18:56:33 +0100

    play stop implemented

--- client/scripts/actions.js
+++ client/scripts/actions.js
@@ -2,6 +2,7 @@ const typeValueAction = type => value => ({type, value});
 
 export const UPDATE_ARPEGGIATOR_IS_ON = 'UPDATE_ARPEGGIATOR_IS_ON';
 export const UPDATE_PATTERN = 'UPDATE_PATTERN';
+export const UPDATE_PLAYING = 'UPDATE_PLAYING';
 export const UPDATE_ROOT_NOTE = 'UPDATE_ROOT_NOTE';
 export const UPDATE_SELECTED_EFFECT = 'UPDATE_SELECTED_EFFECT';
 export const UPDATE_SELECTED_INSTRUMENT = 'UPDATE_SELECTED_INSTRUMENT';
@@ -10,6 +11,7 @@ export const UPDATE_SELECTED_SCALE = 'UPDATE_SELECTED_SCALE';
 
 export const updateArpeggiatorIsOn = typeValueAction(UPDATE_ARPEGGIATOR_IS_ON);
 export const updatePattern = typeValueAction(UPDATE_PATTERN);
+export const updatePlaying = typeValueAction(UPDATE_PLAYING);
 export const updateRootNote = typeValueAction(UPDATE_ROOT_NOTE);
 export const updateSelectedEffect = typeValueAction(UPDATE_SELECTED_EFFECT);
 export const updateSelectedPattern = typeValueAction(UPDATE_SELECTED_PATTERN);
--- client/scripts/components/atoms/PlayButton.js
+++ client/scripts/components/atoms/PlayButton.js
@@ -1,7 +1,15 @@
+/* global R */
 import React from 'react';
+import {connect} from 'react-redux';
+import {updatePlaying} from '../../actions';
 
+const {identity} = R;
+
+@connect(identity)
 export default class extends React.Component {
   render () {
-    return <div className="play-button"></div>;
+    const {dispatch, playing} = this.props;
+    return <div className={`play-button ${playing ? 'selected' : ''}`}
+                onClick={() => dispatch(updatePlaying(!playing))}></div>;
   }
 };
