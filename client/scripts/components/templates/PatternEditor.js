6ed13c055e87e3de0bc5f13ebebccf355b81b028
benji6 <benhall22@hotmail.co.uk>
2015-09-13 10:00:57 +0100

    more progress on pattern editor

--- client/scripts/components/templates/PatternEditor.js
+++ client/scripts/components/templates/PatternEditor.js
@@ -9,8 +9,10 @@ import Navigation from '../organisms/Navigation';
 
 const {identity} = R;
 
-const handleOnClick = (dispatch, pattern, i, j) => () =>
-  dispatch(updatePattern(mapIndexed((row, x) => mapIndexed((cell, y) => x === i && y === j ? !cell : cell,
+const handleOnClick = (dispatch, pattern, i, j) =>
+  dispatch(updatePattern(mapIndexed((row, x) => mapIndexed((cell, y) => x === i && y === j ?
+                                      {...cell, selected: !cell.selected} :
+                                      cell,
                                                            row),
                                     pattern)));
 const bpm = 140;
@@ -18,31 +20,36 @@ const timeInterval = 60000 / bpm;
 
 const playStopSubject = new Rx.Subject();
 
-const onPlay = (dispatch, pattern) => () =>
+const onPlay = (dispatch, pattern) =>
   timer(0, timeInterval)
     .takeUntil(playStopSubject)
     .subscribe(count =>
-      dispatch(updatePattern(mapIndexed(row => mapIndexed((cell, y) => y === count % pattern.length ? !cell : cell,
+      dispatch(updatePattern(mapIndexed(row => mapIndexed((cell, y) => y === count % pattern.length ?
+                                                            {...cell, active: !cell.active} :
+                                                            cell,
                                                           row),
                                         pattern))));
 
-const onStop = (dispatch, pattern) => () => {
+const onStop = (dispatch, pattern) => {
   playStopSubject.onNext();
-  dispatch(updatePattern(mapIndexed(row => mapIndexed(() => false,
+  dispatch(updatePattern(mapIndexed(row => mapIndexed(cell => ({...cell, active: false}),
                                                       row),
                                     pattern)));
 };
+
+const selectedClass = ({selected}) => selected === true ? 'selected' : '';
+const activeClass = ({active}) => active === true ? 'active' : '';
 
 export default connect(identity)(({dispatch, pattern}) =>
   <div>
     <Navigation />
     <div className="pattern-editor">
       {pattern.map((x, i) =>
-        x.map((y, j) =>
-          <div className={y === false ? 'step' : 'step selected'}
+        x.map((cell, j) =>
+          <div className={`step ${selectedClass(cell)} ${activeClass(cell)}`}
                key={`${i}-${j}`}
-               onClick={handleOnClick(dispatch, pattern, i, j)} />))}
+               onClick={() => handleOnClick(dispatch, pattern, i, j)} />))}
     </div>
-    <PlayButton onPlay={onPlay(dispatch, pattern)}
-                onStop={onStop(dispatch, pattern)} />
+    <PlayButton onPlay={() => onPlay(dispatch, pattern)}
+                onStop={() => onStop(dispatch, pattern)} />
   </div>);
--- client/scripts/reducers/pattern.js
+++ client/scripts/reducers/pattern.js
@@ -3,11 +3,6 @@ import {UPDATE_PATTERN} from '../actions';
 
 const {repeat} = R;
 
-export default (state = repeat(repeat(false, 8), 8), {type, value}) => {
-  switch (type) {
-    case UPDATE_PATTERN:
-      return value;
-    default:
-      return state;
-  }
-};
+export default (state = repeat(repeat({selected: false,
+                                       active: false}, 8), 8), {type, value}) =>
+  type === UPDATE_PATTERN ? value : state;
