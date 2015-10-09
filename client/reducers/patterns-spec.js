01de803498893075ed398f8e0785e6e0990eab4a
benji6 <benhall22@hotmail.co.uk>
2015-10-09 07:32:09 +0100

    replace ramda update with destructuring

--- client/reducers/patterns-spec.js
+++ client/reducers/patterns-spec.js
@@ -1,5 +1,4 @@
 import test from 'tape';
-import {update} from 'ramda';
 import reducer, {initialState} from './patterns';
 import {activePatternCellClick,
         updateActivePatternActivePosition,
@@ -18,15 +17,15 @@ test(`${reducerName} reducer handles active pattern cell click`, t => {
   const activePatternIndex = 0;
   const activePattern = initialState[activePatternIndex];
   const value = {x: 1, y: 2};
-  const testState = update(activePatternIndex,
-                           {...activePattern, notes: [value]},
-                           initialState);
+  const testState = [...initialState.slice(0, activePatternIndex),
+                     {...activePattern, notes: [value]},
+                     ...initialState.slice(activePatternIndex + 1)];
   t.deepEqual(reducer(undefined, activePatternCellClick(value)),
               testState);
   t.deepEqual(reducer(testState, activePatternCellClick(value)),
-              update(activePatternIndex,
-                     {...activePattern, notes: []},
-                     initialState));
+              [...initialState.slice(0, activePatternIndex),
+               {...activePattern, notes: []},
+               ...initialState.slice(activePatternIndex + 1)]);
   t.end();
 });
 
@@ -36,9 +35,9 @@ test(`${reducerName} reducer updates active pattern active position`, t => {
   const value = 3;
 
   t.deepEqual(reducer(undefined, updateActivePatternActivePosition(value)),
-              update(activePatternIndex,
-                     {...activePattern, activePosition: value},
-                     initialState));
+              [...initialState.slice(0, activePatternIndex),
+               {...activePattern, activePosition: value},
+               ...initialState.slice(activePatternIndex + 1)]);
   t.end();
 });
 
@@ -48,9 +47,9 @@ test(`${reducerName} reducer updates active pattern instrument`, t => {
   const value = 'tubaphone';
 
   t.deepEqual(reducer(undefined, updateActivePatternInstrument(value)),
-              update(activePatternIndex,
-                     {...activePattern, instrument: value},
-                     initialState));
+              [...initialState.slice(0, activePatternIndex),
+               {...activePattern, instrument: value},
+               ...initialState.slice(activePatternIndex + 1)]);
   t.end();
 });
 
@@ -60,8 +59,8 @@ test(`${reducerName} reducer updates active pattern xLength`, t => {
   const value = 5;
 
   t.deepEqual(reducer(undefined, updateActivePatternXLength(value)),
-              update(activePatternIndex,
-                     {...activePattern, xLength: value},
-                     initialState));
+              [...initialState.slice(0, activePatternIndex),
+               {...activePattern, xLength: value},
+               ...initialState.slice(activePatternIndex + 1)]);
   t.end();
 });
