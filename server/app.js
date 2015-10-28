59ebe1f498e206de578f73c2357f2a3bd002c0df
benji6 <benhall22@hotmail.co.uk>
2015-10-28 12:30:45 +0000

    use express compression middleware

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const compression = require('compression');
+
 const app = express();
 app.use(express.static(`${__dirname}/../public`));
+app.use(compression());
 app.listen(80);
