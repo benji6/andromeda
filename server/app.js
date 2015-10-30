6264ddf35c4130d519c1edcdc486bf835649242b
benji6 <benhall22@hotmail.co.uk>
2015-10-30 09:44:24 +0000

    getting gzip to work depends on the order of calling app.use....

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const compression = require('compression');
 
 const app = express();
+app.use(compression({threshold: 0}));
 app.use(express.static(`${__dirname}/../public`));
-app.use(compression());
 app.listen(80);
