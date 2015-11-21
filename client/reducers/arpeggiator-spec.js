5321cf7c0d039571fa6b2f35ca80c3b9f933036f
benji6 <benhall22@hotmail.co.uk>
2015-11-21 15:49:15 +0000

    tweak

--- client/reducers/arpeggiator-spec.js
+++ client/reducers/arpeggiator-spec.js
@@ -2,15 +2,20 @@ import test from 'tape';
 import reducer, {initialState} from './arpeggiator';
 import {updateArpeggiatorIsOn, updateSelectedPattern} from '../actions';
 
-test('arpeggiator reducer returns initial state', t => {
+const reducerName = 'arpeggiator';
+
+test(`${reducerName} reducer returns initial state`, t => {
   t.deepEqual(reducer(undefined, {}), initialState);
   t.deepEqual(reducer(undefined, {}), initialState);
   t.end();
 });
 
-test('arpeggiator reducer updates state', t => {
+test(`${reducerName} reducer returns updateArpeggiatorIsOn`, t => {
   t.deepEqual(reducer(undefined, updateArpeggiatorIsOn(true)),
               {...initialState, arpeggiatorIsOn: true});
+  t.end();
+});
+test(`${reducerName} reducer returns updateArpeggiatorIsOn`, t => {
   t.deepEqual(reducer(undefined, updateSelectedPattern('down')),
               {...initialState, selectedPattern: 'down'});
   t.end();
