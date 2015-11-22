db967953fb2637dd9bd1a84444ce91a6aeedc734
benji6 <benhall22@hotmail.co.uk>
2015-11-22 12:54:05 +0000

    control-pad is an organism

--- client/components/pages/ControlPadView.js
+++ client/components/pages/ControlPadView.js
@@ -1,7 +1,7 @@
 import {identity} from 'ramda';
 import React from 'react';
 import {connect} from 'react-redux';
-import ControlPad from '../atoms/ControlPad';
+import ControlPad from '../organisms/ControlPad';
 import PerformanceMenu from '../organisms/PerformanceMenu';
 import Navigation from '../organisms/Navigation';
 
