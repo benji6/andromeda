29d6cebac0d8cab63dd7bdd3b0091bc2585568e0
benji6 <benhall22@hotmail.co.uk>
2015-12-06 23:34:30 +0000

    style(Settings)

--- client/components/pages/SettingsView.js
+++ client/components/pages/SettingsView.js
@@ -1,63 +1,99 @@
-import capitalize from 'capitalize';
-import {compose, identity, keys, map, path, tap} from 'ramda';
-import React from 'react';
-import {connect} from 'react-redux';
+import capitalize from 'capitalize'
+import {compose, identity, keys, map, tap} from 'ramda'
+import React from 'react'
+import {connect} from 'react-redux'
 import {updateArpeggiatorIsOn,
         updateBpm,
         updateMicrophoneIsOn,
         updateSelectedRootNote,
         updateSelectedScale,
-        updateSelectedPattern} from '../../actions';
-import Navigation from '../organisms/Navigation';
-import CheckboxSelector from '../molecules/CheckboxSelector';
-import RangeSelector from '../molecules/RangeSelector';
-import noteNameFromPitch from '../../audioHelpers/noteNameFromPitch';
-import Selector from '../molecules/Selector';
-import ArpeggiatorSelector from '../molecules/ArpeggiatorSelector';
-import switchMicrophone from '../../switchMicrophone';
+        updateSelectedPattern} from '../../actions'
+import Navigation from '../organisms/Navigation'
+import CheckboxSelector from '../molecules/CheckboxSelector'
+import RangeSelector from '../molecules/RangeSelector'
+import noteNameFromPitch from '../../audioHelpers/noteNameFromPitch'
+import Selector from '../molecules/Selector'
+import ArpeggiatorSelector from '../molecules/ArpeggiatorSelector'
+import switchMicrophone from '../../switchMicrophone'
 import {
   eventValuePath,
   eventCheckedPath,
 } from '../../tools/paths'
 
-const minBpm = 32;
+const minBpm = 32
 
-export default connect(identity)(({arpeggiator: {arpeggiatorIsOn, patterns, selectedPattern},
-                                   bpm,
-                                   dispatch,
-                                   microphone,
-                                   rootNote,
-                                   scale: {scaleName, scales}}) =>
+export default connect(identity)(({
+  arpeggiator: {arpeggiatorIsOn, patterns, selectedPattern},
+  bpm,
+  dispatch,
+  microphone,
+  rootNote,
+  scale: {scaleName, scales},
+}) =>
   <div className="settings-view">
     <Navigation />
     <h1 className="text-center">Settings</h1>
     <div className="flex-column text-center">
-      <RangeSelector output={bpm}
-                     max="512"
-                     min={minBpm}
-                     onChange={compose(dispatch, updateBpm, Number, path(['target', 'value']))}
-                     text="BPM"
-                     value={bpm} />
-     <RangeSelector max="24"
-                    min="-36"
-                    onChange={compose(dispatch, updateSelectedRootNote, Number, eventValuePath)}
-                    output={noteNameFromPitch(rootNote)}
-                    text="Root Note"
-                    value={rootNote} />
-      <Selector defaultValue={scaleName}
-                handleChange={compose(dispatch, updateSelectedScale, eventValuePath)}
-                label="Scale"
-                options={map(value => ({text: capitalize.words(value),
-                                        value}), keys(scales))} />
-      <CheckboxSelector checked={microphone.isOn}
-                        disabled={!microphone.isAvailable}
-                        onChange={compose(dispatch, updateMicrophoneIsOn, tap(switchMicrophone(dispatch)), eventCheckedPath)}
-                        text="Microphone" />
-      <ArpeggiatorSelector arpeggiatorIsOn={arpeggiatorIsOn}
-                           dispatch={dispatch}
-                           patterns={patterns}
-                           selectedPattern={selectedPattern}
-                           handleArpeggiatorIsOnChange={compose(dispatch, updateArpeggiatorIsOn, eventCheckedPath)}
-                           handlePatternSelect={compose(dispatch, updateSelectedPattern, eventValuePath)} />
+      <RangeSelector
+        max="512"
+        min={minBpm}
+        onChange={compose(
+          dispatch,
+          updateBpm,
+          Number,
+          eventValuePath
+        )}
+        output={bpm}
+        text="BPM"
+        value={bpm}
+      />
+      <RangeSelector
+        max="24"
+        min="-36"
+        onChange={compose(
+          dispatch,
+          updateSelectedRootNote,
+          Number,
+          eventValuePath
+        )}
+        output={noteNameFromPitch(rootNote)}
+        text="Root Note"
+        value={rootNote}
+      />
+      <Selector
+        defaultValue={scaleName}
+        handleChange={compose(dispatch, updateSelectedScale, eventValuePath)}
+        label="Scale"
+        options={map(
+          value => ({text: capitalize.words(value), value}),
+          keys(scales)
+        )} />
+      <CheckboxSelector
+        checked={microphone.isOn}
+        disabled={!microphone.isAvailable}
+        onChange={compose(
+          dispatch,
+          updateMicrophoneIsOn,
+          tap(switchMicrophone(dispatch)),
+          eventCheckedPath
+        )}
+        text="Microphone"
+      />
+      <ArpeggiatorSelector
+        arpeggiatorIsOn={arpeggiatorIsOn}
+        dispatch={dispatch}
+        patterns={patterns}
+        selectedPattern={selectedPattern}
+        handleArpeggiatorIsOnChange={compose(
+          dispatch,
+          updateArpeggiatorIsOn,
+          eventCheckedPath
+        )}
+        handlePatternSelect={compose(
+          dispatch,
+          updateSelectedPattern,
+          eventValuePath
+        )}
+      />
     </div>
-  </div>);
+  </div>)
