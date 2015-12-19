6d6722681efe02a3642bf4ca2ebb3673de724ffd
benji6 <benhall22@hotmail.co.uk>
2015-12-19 12:45:05 +0000

    pattern steps should be buttons

--- client/components/molecules/PatternRow.js
+++ client/components/molecules/PatternRow.js
@@ -7,7 +7,9 @@ const activeClass = ({active}) => active === true ? 'active' : ''
 export default ({cells, onClick, yLabel}) =>
   <div className="flex-row pattern-row">
     <div className="y-label" key={"flex-row-y-label"}>{yLabel}</div>
-    {mapIndexed((cell, i) => <div className={`step ${selectedClass(cell)} ${activeClass(cell)}`}
-                                  key={`flex-row-${i}`}
-                                  onClick={onClick(i)} />, cells)}
+    {mapIndexed((cell, i) => <button
+      className={`step ${selectedClass(cell)} ${activeClass(cell)}`}
+      key={`flex-row-${i}`}
+      onClick={onClick(i)}
+    />, cells)}
   </div>
