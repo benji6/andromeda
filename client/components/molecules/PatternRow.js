77423c3647b6c4d658f4f82e21fff5410f7e226d
benji6 <benhall22@hotmail.co.uk>
2015-12-19 20:25:03 +0000

    step Atom

--- client/components/molecules/PatternRow.js
+++ client/components/molecules/PatternRow.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {mapIndexed} from '../../tools/indexedIterators'
+import Step from '../atoms/Step'
 
 const selectedClass = ({selected}) => selected === true ? 'selected' : ''
 const activeClass = ({active}) => active === true ? 'active' : ''
@@ -7,7 +8,7 @@ const activeClass = ({active}) => active === true ? 'active' : ''
 export default ({cells, onClick, yLabel}) =>
   <div className="flex-row pattern-row">
     <div className="y-label" key={"flex-row-y-label"}>{yLabel}</div>
-    {mapIndexed((cell, i) => <button
+    {mapIndexed((cell, i) => <Step
       className={`step ${selectedClass(cell)} ${activeClass(cell)}`}
       key={`flex-row-${i}`}
       onClick={onClick(i)}
