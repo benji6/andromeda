1dc5e8b816e04ef1ac8b4b2c660605272f1dd7b9
benji6 <benhall22@hotmail.co.uk>
2015-12-28 10:26:47 +0000

    fix random arp pattern

--- client/reducers/arpeggiatorPatterns.js
+++ client/reducers/arpeggiatorPatterns.js
@@ -1,8 +1,10 @@
-import {makeCircular, iterate} from 'imlazy'
+import {makeCircular} from 'imlazy'
 import {compose, concat, nth, reverse} from 'ramda'
 
 export const initialState = {
-  random: xs => iterate(_ => nth(Math.random() * (xs.length - 1), xs)),
+  random: xs => ({[Symbol.iterator]: function * () {
+    while (true) yield nth(Math.floor(Math.random() * xs.length), xs)
+  }}),
   up: makeCircular,
   down: compose(makeCircular, reverse),
   'up and down': xs => makeCircular(concat(xs, reverse(xs)))
