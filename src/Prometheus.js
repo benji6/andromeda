f772f45b533347ec1c41991f31750d2840ef7bef
benji6 <benhall22@hotmail.co.uk>
2016-02-04 13:49:00 +0000

    instrument plugin has connect and disconnect interface

--- src/Prometheus.js
+++ src/Prometheus.js
@@ -1,13 +1,25 @@
 import {dissoc} from 'ramda'
 import createVirtualAudioGraph from 'virtual-audio-graph'
 
+const outputs = new WeakMap()
 const nodes = new WeakMap()
 const virtualAudioGraph = new WeakMap()
 
 export default class {
   constructor ({audioContext}) {
+    const output = audioContext.createGain()
+    outputs.set(this, output)
     nodes.set(this, {})
-    virtualAudioGraph.set(this, createVirtualAudioGraph({audioContext}))
+    virtualAudioGraph.set(this, createVirtualAudioGraph({
+      audioContext,
+      output
+    }))
+  }
+  connect (destination) {
+    outputs.get(this).connect(destination)
+  }
+  disconnect (destination) {
+    outputs.get(this).disconnect()
   }
   inputNoteStart ({frequency, gain, id, startTime, stopTime}) {
     const newNodes = {
