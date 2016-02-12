d1bcf51136d7806239bd7b834701658331132faf
benji6 <benhall22@hotmail.co.uk>
2016-02-12 08:06:32 +0000

    instantiate osc

--- src/loadPlugins.js
+++ src/loadPlugins.js
@@ -16,6 +16,7 @@ store.dispatch(loadPluginInstrument({constructor: Osc, name: 'osc'}))
 store.dispatch(loadPluginEffect({constructor: Delay, name: 'delay'}))
 store.dispatch(loadPluginEffect({constructor: Gain, name: 'gain'}))
 store.dispatch(instantiateInstrument({name: 'Prometheus', plugin: 'prometheus'}))
+store.dispatch(instantiateInstrument({name: 'Osc', plugin: 'osc'}))
 
 store.dispatch(instantiateEffect({channel: 0, name: 'delay', plugin: 'delay'}))
 
