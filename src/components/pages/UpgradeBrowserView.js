91c32878a8890c8422d397e03e351e60b0fdfe86
benji6 <benhall22@hotmail.co.uk>
2016-02-12 08:07:29 +0000

    style tweak

--- src/components/pages/UpgradeBrowserView.js
+++ src/components/pages/UpgradeBrowserView.js
@@ -1,4 +1,6 @@
 import React from 'react'
-export default () => <div className='flex-column text-center'>
-  <h1>Requires the latest version of Chrome to run...</h1>
-</div>
+
+export default _ =>
+  <div className='flex-column text-center'>
+    <h1>Requires the latest version of Chrome to run...</h1>
+  </div>
