70e755f029ae378d6d96ba9a418f52bb847972c5
benji6 <benhall22@hotmail.co.uk>
2016-02-13 11:44:13 +0000

    osc has been removed and Prometheus has absorbed its functionality

--- src/loadPlugins.js
+++ src/loadPlugins.js
@@ -2,7 +2,6 @@ import store from './store'
 import Ariadne from './plugins/instruments/Ariadne'
 import Fate from './plugins/instruments/Fate'
 import Prometheus from './plugins/instruments/Prometheus'
-import Osc from './plugins/instruments/Osc'
 import Delay from './plugins/effects/Delay'
 import Gain from './plugins/effects/Gain'
 import {
@@ -15,16 +14,14 @@ import {
 
 store.dispatch(loadPluginInstrument({constructor: Ariadne, name: 'Ariadne'}))
 store.dispatch(loadPluginInstrument({constructor: Fate, name: 'Fate'}))
-store.dispatch(loadPluginInstrument({constructor: Prometheus, name: 'prometheus'}))
-store.dispatch(loadPluginInstrument({constructor: Osc, name: 'osc'}))
+store.dispatch(loadPluginInstrument({constructor: Prometheus, name: 'Prometheus'}))
 
 store.dispatch(loadPluginEffect({constructor: Delay, name: 'delay'}))
 store.dispatch(loadPluginEffect({constructor: Gain, name: 'gain'}))
 
 store.dispatch(instantiateInstrument({name: 'Ariadne', plugin: 'Ariadne'}))
 store.dispatch(instantiateInstrument({name: 'Fate', plugin: 'Fate'}))
-store.dispatch(instantiateInstrument({name: 'Prometheus', plugin: 'prometheus'}))
-store.dispatch(instantiateInstrument({name: 'Osc', plugin: 'osc'}))
+store.dispatch(instantiateInstrument({name: 'Prometheus', plugin: 'Prometheus'}))
 
 store.dispatch(instantiateEffect({channel: 0, name: 'delay', plugin: 'delay'}))
 
--- src/plugins/instruments/Prometheus.js
+++ src/plugins/instruments/Prometheus.js
@@ -1,15 +1,19 @@
 import {dissoc} from 'ramda'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import createVirtualAudioGraph from 'virtual-audio-graph'
 
 const graphs = new WeakMap()
 const outputs = new WeakMap()
 const virtualAudioGraphs = new WeakMap()
+const types = new WeakMap()
 
 export default class {
   constructor ({audioContext}) {
     const output = audioContext.createGain()
     outputs.set(this, output)
     graphs.set(this, {})
+    types.set(this, 'sine')
     virtualAudioGraphs.set(this, createVirtualAudioGraph({
       audioContext,
       output
@@ -27,7 +31,7 @@ export default class {
       0: ['gain', 'output', {gain}],
       [id]: ['oscillator', 0, {
         frequency,
-        type: 'sine',
+        type: types.get(this),
         startTime,
         stopTime
       }]
@@ -40,7 +44,24 @@ export default class {
     graphs.set(this, newGraph)
     virtualAudioGraphs.get(this).update(newGraph)
   }
-  render (x) {
-    console.log(x)
+  render (containerEl) {
+    ReactDOM.render(
+      <div style={{textAlign: 'center'}}>
+        <h2>Prometheus</h2>
+        <label>
+          Type&nbsp;
+          <select
+            defaultValue={types.get(this)}
+            onChange={e => types.set(this, e.target.value)}
+          >
+            <option value='sawtooth'>Sawtooth</option>
+            <option value='sine'>Sine</option>
+            <option value='square'>Square</option>
+            <option value='triangle'>Triangle</option>
+          </select>
+        </label>
+      </div>,
+      containerEl
+    )
   }
 }
