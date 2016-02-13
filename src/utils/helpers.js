d60cfc2acf5af47c671d45d113b0026f647b418b
benji6 <benhall22@hotmail.co.uk>
2016-02-13 20:57:16 +0000

    remove dead code - paramsStartTimePath and paramsStopTimePath

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21,7 +21,5 @@ export const eventValuePath = path(['currentTarget', 'value'])
 export const forEachIndexed = addIndex(forEach)
 export const lazyMapIndexed = addIndex(lazyMap)
 export const mapIndexed = addIndex(map)
-export const paramsStartTimePath = path(['params', 'startTime'])
-export const paramsStopTimePath = path(['params', 'stopTime'])
 export const randomElement = xs => xs[Math.floor(Math.random() * xs.length)]
 export const reduceIndexed = addIndex(reduce)
