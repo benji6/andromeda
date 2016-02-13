087bcc875a0e7292050a72b37377f1e462097e15
benji6 <benhall22@hotmail.co.uk>
2016-02-13 11:41:30 +0000

    add Fate instrument plugin

--- src/loadPlugins.js
+++ src/loadPlugins.js
@@ -1,5 +1,6 @@
 import store from './store'
 import Ariadne from './plugins/instruments/Ariadne'
+import Fate from './plugins/instruments/Fate'
 import Prometheus from './plugins/instruments/Prometheus'
 import Osc from './plugins/instruments/Osc'
 import Delay from './plugins/effects/Delay'
@@ -13,11 +14,15 @@ import {
 } from './actions'
 
 store.dispatch(loadPluginInstrument({constructor: Ariadne, name: 'Ariadne'}))
+store.dispatch(loadPluginInstrument({constructor: Fate, name: 'Fate'}))
 store.dispatch(loadPluginInstrument({constructor: Prometheus, name: 'prometheus'}))
 store.dispatch(loadPluginInstrument({constructor: Osc, name: 'osc'}))
+
 store.dispatch(loadPluginEffect({constructor: Delay, name: 'delay'}))
 store.dispatch(loadPluginEffect({constructor: Gain, name: 'gain'}))
+
 store.dispatch(instantiateInstrument({name: 'Ariadne', plugin: 'Ariadne'}))
+store.dispatch(instantiateInstrument({name: 'Fate', plugin: 'Fate'}))
 store.dispatch(instantiateInstrument({name: 'Prometheus', plugin: 'prometheus'}))
 store.dispatch(instantiateInstrument({name: 'Osc', plugin: 'osc'}))
 
---
+++ src/plugins/instruments/Fate.js
@@ -0,0 +1,118 @@
+import {assoc, dissoc, map, range, zipObj} from 'ramda'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import createVirtualAudioGraph from 'virtual-audio-graph'
+
+const graphs = new WeakMap()
+const outputs = new WeakMap()
+const virtualAudioGraphs = new WeakMap()
+const oscTypes = new WeakMap()
+const oscTotals = new WeakMap()
+const detunes = new WeakMap()
+
+export default class {
+  constructor ({audioContext}) {
+    const output = audioContext.createGain()
+
+    outputs.set(this, output)
+    graphs.set(this, {})
+    oscTypes.set(this, 'sawtooth')
+    oscTotals.set(this, 7)
+    detunes.set(this, 12)
+
+    const virtualAudioGraph = createVirtualAudioGraph({
+      audioContext,
+      output
+    })
+
+    virtualAudioGraph.defineNodes({
+      oscBank: ({gain, frequency, startTime, stopTime}) => {
+        const totalOscillators = oscTotals.get(this)
+        const osc = i => ['oscillator', 0, {
+          detune: (i - Math.floor(totalOscillators / 2)) * detunes.get(this),
+          frequency,
+          startTime,
+          stopTime,
+          type: oscTypes.get(this)
+        }]
+        const oscillators = zipObj(
+          range(1, totalOscillators + 1),
+          map(osc, range(0, totalOscillators))
+        )
+        return assoc(0, ['gain', 'output', {gain}], oscillators)
+      }
+    })
+    virtualAudioGraphs.set(this, virtualAudioGraph)
+  }
+  connect (destination) {
+    outputs.get(this).connect(destination)
+  }
+  disconnect (destination) {
+    outputs.get(this).disconnect()
+  }
+  inputNoteStart ({frequency, gain, id, startTime, stopTime}) {
+    const newNodes = {
+      ...graphs.get(this),
+      [id]: ['oscBank', 'output', {
+        frequency,
+        gain,
+        oscType: oscTypes.get(this),
+        startTime,
+        stopTime
+      }]
+    }
+    graphs.set(this, newNodes)
+    virtualAudioGraphs.get(this).update(newNodes)
+  }
+  inputNoteStop (id) {
+    const newGraph = dissoc(id, graphs.get(this))
+    graphs.set(this, newGraph)
+    virtualAudioGraphs.get(this).update(newGraph)
+  }
+  render (containerEl) {
+    ReactDOM.render(
+      <div style={{textAlign: 'center'}}>
+        <h2>Fate</h2>
+        <p>
+          <label>
+            Osc type&nbsp;
+            <select
+              defaultValue={oscTypes.get(this)}
+              onChange={e => oscTypes.set(this, e.target.value)}
+            >
+              <option value='sawtooth'>Sawtooth</option>
+              <option value='sine'>Sine</option>
+              <option value='square'>Square</option>
+              <option value='triangle'>Triangle</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Detune&nbsp;
+            <input
+              defaultValue={detunes.get(this)}
+              max='50'
+              min='0'
+              onInput={e => detunes.set(this, Number(e.target.value))}
+              type='range'
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            totalOscillators&nbsp;
+            <input
+              defaultValue={oscTotals.get(this)}
+              max='50'
+              min='1'
+              onInput={e => oscTotals.set(this, Number(e.target.value))}
+              type='range'
+            />
+          </label>
+        </p>
+      </div>,
+      containerEl
+    )
+  }
+}
