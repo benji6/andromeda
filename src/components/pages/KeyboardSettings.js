2d549630781e1b93b68271f0d3c2e3c5e408cb65
benji6 <benhall22@hotmail.co.uk>
2016-02-13 20:10:49 +0000

    style(Keyboard-Settings)

--- src/components/pages/KeyboardSettings.js
+++ src/components/pages/KeyboardSettings.js
@@ -12,35 +12,41 @@ import FullButton from '../atoms/FullButton'
 import RangeSelector from '../molecules/RangeSelector'
 import {eventValuePath} from '../../utils/helpers'
 
-export default connect(identity)(({keyboard, dispatch, instruments}) => <div className='flex-column text-center'>
+export default connect(identity)(({keyboard, dispatch, instruments}) =>
+  <div className='flex-column text-center'>
     <h2 className='text-center'>Keyboard Settings</h2>
     <Selector
-  defaultValue={keyboard.instrument}
-  handleChange={(compose(dispatch, updateKeyboardInstrument, eventValuePath))}
-  label='Instrument'
-  options={map(
-    instrument => ({text: capitalize.words(instrument), value: instrument}),
-    keys(instruments)
-  )}
-  />
+      defaultValue={keyboard.instrument}
+      handleChange={(compose(
+        dispatch,
+        updateKeyboardInstrument,
+        eventValuePath
+      ))}
+      label='Instrument'
+      options={map(
+        instrument => ({text: capitalize.words(instrument), value: instrument}),
+        keys(instruments)
+      )}
+    />
     <RangeSelector
-  max='1'
-  min='0'
-  step='0.01'
-  onChange={compose(dispatch, updateKeyboardVolume, Number, eventValuePath)}
-  output={Math.round(keyboard.volume * 100)}
-  text='Volume'
-  value={keyboard.volume}
-  />
+      max='1'
+      min='0'
+      step='0.01'
+      onChange={compose(dispatch, updateKeyboardVolume, Number, eventValuePath)}
+      output={Math.round(keyboard.volume * 100)}
+      text='Volume'
+      value={keyboard.volume}
+    />
     <RangeSelector
-  max='2'
-  min='-3'
-  onChange={compose(dispatch, updateKeyboardOctave, Number, eventValuePath)}
-  output={keyboard.octave}
-  text='Octave'
-  value={keyboard.octave}
-  />
+      max='2'
+      min='-3'
+      onChange={compose(dispatch, updateKeyboardOctave, Number, eventValuePath)}
+      output={keyboard.octave}
+      text='Octave'
+      value={keyboard.octave}
+    />
     <div>
-      <span className='inline-label-text'></span><FullButton text='OK' to='/settings' />
+      <span className='inline-label-text'></span>
+      <FullButton text='OK' to='/settings' />
     </div>
   </div>)
