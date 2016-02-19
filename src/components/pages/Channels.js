a775f2fe913ed36a563234c727e8e3d57246189c
benji6 <benhall22@hotmail.co.uk>
2016-02-19 08:50:57 +0000

    feat(channels) channels view now represents plugins state

--- src/components/pages/Channels.js
+++ src/components/pages/Channels.js
@@ -1,25 +1,25 @@
-import {always, compose, map} from 'ramda'
+import {map, pluck, uniq} from 'ramda'
 import React from 'react'
 import {rawConnect} from '../../utils/helpers'
-import {addChannel, removeChannel} from '../../actions'
 import Navigation from '../organisms/Navigation'
 import FullButton from '../atoms/FullButton'
 import {Cross, Plus} from '../atoms/IconButtons'
 
-export default rawConnect(({dispatch}) => <div>
+export default rawConnect(({plugins: {effectInstances}}) =>
+  <div>
     <Navigation />
     <div className='flex-column text-center'>
       <h1>Channels</h1>
       {map(
-        ({id}) => <div key={id}>
-          <FullButton text={`Channel ${id}`} to={`/channel/${id}`} />
-          <Cross onClick={compose(dispatch, removeChannel, always(id))}/>
+        channel => <div key={channel}>
+          <FullButton text={`Channel ${channel}`} to={`/channel/${channel}`} />
+          <Cross />
         </div>,
-        []
+        uniq(pluck('channel', effectInstances))
       )}
-        <div>
-          Add New Channel
-          <Plus onClick={compose(dispatch, addChannel)}/>
-        </div>
+      <div>
+        Add New Channel
+        <Plus />
+      </div>
     </div>
   </div>)
