0c6232d72b13507ab21f8de17a8c192f44aad90b
benji6 <benhall22@hotmail.co.uk>
2016-02-21 12:42:21 +0000

    feat(Channel) render instruments and effects on Channel page

--- src/components/pages/Channel.js
+++ src/components/pages/Channel.js
@@ -1,13 +1,32 @@
+import {map, nth, prop} from 'ramda'
 import React from 'react'
+import FullButton from '../atoms/FullButton'
 import {rawConnect} from '../../utils/helpers'
 
 export default rawConnect(({
-  params
+  params,
+  plugins: {channels}
 }) => {
   const channelId = Number(params.channelId)
   return <div className='flex-column text-center justify-center'>
     <h1>{`Channel ${channelId}`}</h1>
     <h2>Sources</h2>
+    <div className='text-center'>
+      {map(
+        x => <FullButton key={x} to={`/plugins/instruments/${x}`}>
+          {x}
+        </FullButton>,
+        prop('instruments', nth(params.channelId, channels))
+      )}
+    </div>
     <h2>Effects</h2>
+      <div className='text-center'>
+        {map(
+          x => <FullButton key={x} to={`/plugins/effects/${x}`}>
+            {x}
+          </FullButton>,
+          prop('effects', nth(params.channelId, channels))
+        )}
+      </div>
   </div>
 })
