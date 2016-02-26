174a375acde9635736a2cffc2115646d91eb385d
benji6 <benhall22@hotmail.co.uk>
2016-02-26 21:12:35 +0000

    feat(channel) can add sources to a channel

--- src/components/pages/Channel.js
+++ src/components/pages/Channel.js
@@ -1,32 +1,56 @@
-import {map, nth, prop} from 'ramda'
+import {difference, head, isEmpty, map, nth, pluck, prop, tail} from 'ramda'
 import React from 'react'
-import FullButton from '../atoms/FullButton'
+import {addInstrumentToChannel} from '../../actions'
 import {rawConnect} from '../../utils/helpers'
+import FullButton from '../atoms/FullButton'
+import {Plus} from '../atoms/IconButtons'
+import FullSelect from '../atoms/FullSelect'
+
+let selectedAddSource = null
 
 export default rawConnect(({
+  dispatch,
   params,
-  plugins: {channels}
+  plugins: {channels, instrumentInstances}
 }) => {
   const channelId = Number(params.channelId)
+  const sources = prop('instruments', nth(params.channelId, channels))
+  const addSources = difference(pluck('name', instrumentInstances), sources)
+  selectedAddSource = selectedAddSource || head(addSources)
   return <div className='flex-column text-center justify-center'>
     <h1>{`Channel ${channelId}`}</h1>
     <h2>Sources</h2>
-    <div className='text-center'>
-      {map(
-        x => <FullButton key={x} to={`/plugins/instruments/${x}`}>
-          {x}
-        </FullButton>,
-        prop('instruments', nth(params.channelId, channels))
-      )}
-    </div>
-    <h2>Effects</h2>
-      <div className='text-center'>
         {map(
-          x => <FullButton key={x} to={`/plugins/effects/${x}`}>
-            {x}
-          </FullButton>,
+          x => <div className='text-center' key={x}>
+            <FullButton to={`/plugins/instruments/${x}`}>
+              {x}
+            </FullButton>
+          </div>,
+        sources
+      )}
+    {!isEmpty(addSources) && <p>Add source</p>}
+    {!isEmpty(addSources) && <div>
+      <FullSelect
+        defaultValue={selectedAddSource}
+        onChange={e => selectedAddSource = e.target.value}
+        options={map(text => ({text, value: text}), addSources)}
+      />
+      <Plus onClick={e => {
+        dispatch(addInstrumentToChannel({
+          channel: channelId,
+          name: selectedAddSource
+        }))
+        selectedAddSource = head(tail(addSources))
+      }}/>
+    </div>}
+    <h2>Effects</h2>
+        {map(
+          x => <div key={x} className='text-center'>
+            <FullButton to={`/plugins/effects/${x}`}>
+              {x}
+            </FullButton>
+          </div>,
           prop('effects', nth(params.channelId, channels))
         )}
-      </div>
   </div>
 })
