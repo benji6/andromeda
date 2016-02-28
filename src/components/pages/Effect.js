2adf4a9e7509d638ffd49b78a53ed50d405b52e2
benji6 <benhall22@hotmail.co.uk>
2016-02-28 14:53:38 +0000

    feat(effect) user can go back when editing an effect plugin

--- src/components/pages/Effect.js
+++ src/components/pages/Effect.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {rawConnect} from '../../utils/helpers'
 import {effectInstance} from '../../utils/derivedData'
+import FullButton from '../atoms/FullButton'
 import PluginMount from '../atoms/PluginMount'
 
 export default rawConnect(({
-  plugins,
-  params
-}) => <PluginMount
-  instance={effectInstance(params.name, plugins)}
-/>)
+  history,
+  params,
+  plugins
+}) => <div>
+  <PluginMount instance={effectInstance(params.name, plugins)}/>
+  <div className='margin-bottom' />
+  <div className='text-center'>
+    <FullButton onClick={history.goBack}>OK</FullButton>
+  </div>
+</div>)
