bb7eacc6ce44b856c421bafe12c29c13c104143d
benji6 <benhall22@hotmail.co.uk>
2016-02-28 14:03:48 +0000

    bug(plugins) fix propTypes issue for PluginMount

--- src/components/atoms/PluginMount.js
+++ src/components/atoms/PluginMount.js
@@ -1,9 +1,8 @@
-import React, {PropTypes} from 'react'
+import React from 'react'
 
 export default class extends React.Component {
-  static propTypes = {instance: PropTypes.obj};
   componentDidMount () {
-    this.props.instance.render(this.refs.mount)
+    this.props.instance.render(this.refs.mount) // eslint-disable-line
   }
   render () {
     return <div ref='mount' />
