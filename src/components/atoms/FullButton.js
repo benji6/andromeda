8520afc0b445c26653aee1c0db48ba211d2b1386
benji6 <benhall22@hotmail.co.uk>
2016-02-28 14:49:47 +0000

    feat(instrument) user can go back when editing an instrument plugin

--- src/components/atoms/FullButton.js
+++ src/components/atoms/FullButton.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router'
-export default ({children, ...props}) =>
-  <Link className='full-button' {...props}>{children}</Link>
+export default ({children, ...props}) => props.to
+  ? <Link className='full-button' {...props}>{children}</Link>
+  : <a className='full-button' href='#' {...props}>{children}</a>
--- src/components/pages/Instrument.js
+++ src/components/pages/Instrument.js
@@ -2,10 +2,16 @@ import React from 'react'
 import {rawConnect} from '../../utils/helpers'
 import {instrumentInstance} from '../../utils/derivedData'
 import PluginMount from '../atoms/PluginMount'
+import FullButton from '../atoms/FullButton'
 
 export default rawConnect(({
-  plugins,
-  params
-}) => <PluginMount
-  instance={instrumentInstance(params.name, plugins)}
-/>)
+  history,
+  params,
+  plugins
+}) => <div>
+  <PluginMount instance={instrumentInstance(params.name, plugins)}/>
+  <div className='margin-bottom' />
+  <div className='text-center'>
+    <FullButton onClick={history.goBack}>OK</FullButton>
+  </div>
+</div>)
