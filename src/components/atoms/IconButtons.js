f7c1ff0e4d6c4001e5856bbf969c7aafc4d6261a
benji6 <benhall22@hotmail.co.uk>
2016-03-26 15:24:49 +0000

    feat(icon-buttons) now have hover colors

--- src/components/atoms/IconButtons.js
+++ src/components/atoms/IconButtons.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const createButton = text => ({...props}) =>
-  <button className='icon-button' {...props}>{text}</button>
+const createButton = (modifier, text) => ({...props}) =>
+  <button className={`icon-button icon-button--${modifier}`} {...props}>
+    {text}
+  </button>
 
-export const Cross = createButton('x')
-export const Down = createButton('\u2193')
-export const Plus = createButton('+')
-export const Up = createButton('\u2191')
+export const Cross = createButton('red-hover', 'x')
+export const Down = createButton('blue-hover', '\u2193')
+export const Plus = createButton('green-hover', '+')
+export const Up = createButton('blue-hover', '\u2191')
