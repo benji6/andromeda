a703eb04cb685e291564f72308b2425997c8712d
benji6 <benhall22@hotmail.co.uk>
2016-04-16 10:41:52 +0100

    feat(welcome-message) add WelcomeMessage

---
+++ src/components/atoms/WelcomeMessage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default ({children}) => <div {...{
+  className: 'WelcomeMessage fade-out',
+  children,
+}} />
--- src/components/templates/App.js
+++ src/components/templates/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Navigation from '../organisms/Navigation'
+import {version} from '../../../package'
+import WelcomeMessage from '../atoms/WelcomeMessage'
 
 export default ({children}) =>
   <div>
+    <WelcomeMessage>{`v${version}`}</WelcomeMessage>
     <Navigation />
     {children}
   </div>
