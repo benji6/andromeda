2bb1e2555be77d9a5777b8b948eb42029170281a
benji6 <benhall22@hotmail.co.uk>
2016-04-16 09:43:33 +0100

    feat(Ariadne) loads of work

--- src/plugins/instruments/Ariadne.js
+++ src/plugins/instruments/Ariadne.js
@@ -1,39 +1,75 @@
-import {dissoc} from 'ramda'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import createVirtualAudioGraph from 'virtual-audio-graph'
 
+const carrierDetunes = new WeakMap()
+const carrierOscTypes = new WeakMap()
 const graphs = new WeakMap()
+const masterGains = new WeakMap()
+const modulatorDetunes = new WeakMap()
+const modulatorOscTypes = new WeakMap()
+const modulatorRatios = new WeakMap()
+const notes = new WeakMap()
 const outputs = new WeakMap()
 const virtualAudioGraphs = new WeakMap()
-const oscTypes = new WeakMap()
-const modulatorRatios = new WeakMap()
+
+const notesToGraph = notes => notes.reduce((acc, {
+  frequency, gain, id, startTime, stopTime
+}) => ({
+  ...acc,
+  [id]: ['oscBank', 'output', {
+    frequency,
+    gain,
+    startTime,
+    stopTime
+  }],
+}), {})
+
+const updateAudio = function () {
+  virtualAudioGraphs.get(this).update(notesToGraph(notes.get(this)))
+}
 
 const ControlContainer = ({children}) => <div style={{padding: '1rem'}}>
-  {children}
+  <label>
+    {children}
+  </label>
 </div>
 
 export default class {
   constructor ({audioContext}) {
     const output = audioContext.createGain()
 
-    outputs.set(this, output)
+    carrierDetunes.set(this, 0)
+    carrierOscTypes.set(this, 'sine')
     graphs.set(this, {})
-    oscTypes.set(this, 'sine')
+    masterGains.set(this, 1)
+    modulatorDetunes.set(this, 0)
+    modulatorOscTypes.set(this, 'sine')
     modulatorRatios.set(this, 0.5)
+    notes.set(this, [])
+    outputs.set(this, output)
 
     const virtualAudioGraph = createVirtualAudioGraph({audioContext, output})
 
     virtualAudioGraph.defineNodes({
-      oscBank: ({gain, frequency, oscType, startTime, stopTime}) => ({
-        0: ['gain', ['output'], {gain}],
-        1: ['oscillator', 0, {frequency, startTime, stopTime, type: oscType}],
+      oscBank: ({gain, frequency, startTime, stopTime}) => ({
+        masterGain: ['gain', ['output'], {gain: masterGains.get(this)}],
+        0: ['gain', ['masterGain'], {gain}],
+        1: ['oscillator', 0, {
+          detune: carrierDetunes.get(this),
+          frequency,
+          startTime,
+          stopTime,
+          type: carrierOscTypes.get(this,
+        )}],
         2: ['gain', {key: 1, destination: 'frequency'}, {gain: 1024}],
         3: ['oscillator', 2, {
+          detune: modulatorDetunes.get(this),
           frequency: frequency * modulatorRatios.get(this),
+          type: modulatorOscTypes.get(this),
           startTime,
-          stopTime
-        }]
+          stopTime,
+        }],
       })
     })
     virtualAudioGraphs.set(this, virtualAudioGraph)
@@ -44,55 +80,115 @@ export default class {
   disconnect (destination) {
     outputs.get(this).disconnect(destination)
   }
-  noteStart ({frequency, gain, id, startTime, stopTime}) {
-    const newNodes = {
-      ...graphs.get(this),
-      [id]: ['oscBank', 'output', {
-        frequency,
-        gain,
-        oscType: oscTypes.get(this),
-        startTime,
-        stopTime
-      }]
-    }
-    graphs.set(this, newNodes)
-    virtualAudioGraphs.get(this).update(newNodes)
+  noteStart (note) {
+    const newNotes = [...notes.get(this), note]
+    notes.set(this, newNotes)
+    updateAudio.call(this)
+  }
+  noteModify (note) {
+    const currentNotes = notes.get(this)
+    const extantNoteIdx = currentNotes.findIndex(({id}) => id === note.id)
+    notes.set(this, [
+      ...currentNotes.slice(0, extantNoteIdx),
+      note,
+      ...currentNotes.slice(extantNoteIdx + 1),
+    ])
+    updateAudio.call(this)
   }
   noteStop (id) {
-    const newGraph = dissoc(id, graphs.get(this))
-    graphs.set(this, newGraph)
-    virtualAudioGraphs.get(this).update(newGraph)
+    const newNotes = notes.get(this).filter(note => note.id !== id)
+    notes.set(this, newNotes)
+    updateAudio.call(this)
   }
   render (containerEl) {
     ReactDOM.render(
       <div style={{textAlign: 'center'}}>
         <h2>Ariadne</h2>
         <ControlContainer>
-          <label>
-            osc type&nbsp;
-            <select
-              defaultValue={oscTypes.get(this)}
-              onChange={e => oscTypes.set(this, e.target.value)}
-            >
-              <option value='sawtooth'>Sawtooth</option>
-              <option value='sine'>Sine</option>
-              <option value='square'>Square</option>
-              <option value='triangle'>Triangle</option>
-            </select>
-          </label>
+          Gain&nbsp;
+          <input
+            defaultValue={masterGains.get(this)}
+            max='1.25'
+            min='0'
+            onInput={e => {
+              masterGains.set(this, Number(e.target.value))
+              updateAudio.call(this)
+            }}
+            step='0.01'
+            type='range'
+          />
         </ControlContainer>
         <ControlContainer>
-          <label>
-            modulator frequency&nbsp;
-            <input
-              defaultValue={modulatorRatios.get(this)}
-              max='8'
-              min='0.1'
-              onInput={e => modulatorRatios.set(this, Number(e.target.value))}
-              step='0.1'
-              type='range'
-            />
-          </label>
+          Carrier wave&nbsp;
+          <select
+            defaultValue={carrierOscTypes.get(this)}
+            onChange={e => {
+              carrierOscTypes.set(this, e.target.value)
+              updateAudio.call(this)
+            }}
+          >
+            <option value='sawtooth'>Sawtooth</option>
+            <option value='sine'>Sine</option>
+            <option value='square'>Square</option>
+            <option value='triangle'>Triangle</option>
+          </select>
+        </ControlContainer>
+        <ControlContainer>
+          Modulator wave&nbsp;
+          <select
+            defaultValue={modulatorOscTypes.get(this)}
+            onChange={e => {
+              modulatorOscTypes.set(this, e.target.value)
+              updateAudio.call(this)
+            }}
+          >
+            <option value='sawtooth'>Sawtooth</option>
+            <option value='sine'>Sine</option>
+            <option value='square'>Square</option>
+            <option value='triangle'>Triangle</option>
+          </select>
+        </ControlContainer>
+        <ControlContainer>
+          Carrier detune&nbsp;
+          <input
+            defaultValue={carrierDetunes.get(this)}
+            max='32'
+            min='-32'
+            onInput={e => {
+              carrierDetunes.set(this, Number(e.target.value))
+              updateAudio.call(this)
+            }}
+            step='0.1'
+            type='range'
+          />
+        </ControlContainer>
+        <ControlContainer>
+          Modulator freq&nbsp;
+          <input
+            defaultValue={modulatorRatios.get(this)}
+            max='8'
+            min='0.1'
+            onInput={e => {
+              modulatorRatios.set(this, Number(e.target.value))
+              updateAudio.call(this)
+            }}
+            step='0.1'
+            type='range'
+          />
+        </ControlContainer>
+        <ControlContainer>
+          Modulator detune&nbsp;
+          <input
+            defaultValue={modulatorDetunes.get(this)}
+            max='128'
+            min='-128'
+            onInput={e => {
+              modulatorDetunes.set(this, Number(e.target.value))
+              updateAudio.call(this)
+            }}
+            step='0.1'
+            type='range'
+          />
         </ControlContainer>
       </div>,
       containerEl
