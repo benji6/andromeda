fd67576729e4d8d2e548e62191e53f1e5fb17655
benji6 <benhall22@hotmail.co.uk>
2016-05-14 15:37:34 +0100

    feat(about) add about page

--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -5,6 +5,7 @@ export default _ =>
   <nav className='navigation'>
     <HollowButton to='/controllers/control-pad'>Pad</HollowButton>
     <HollowButton to='/controllers/song'>Song</HollowButton>
-    <HollowButton to='/channels'>Channels</HollowButton>
+    <HollowButton to='/channels'>FX</HollowButton>
     <HollowButton to='/settings'>Settings</HollowButton>
+    <HollowButton to='/about'>?</HollowButton>
   </nav>
