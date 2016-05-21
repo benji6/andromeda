89b841ce8238f23cd4f202f3b5b9413a39da8632
benji6 <benhall22@hotmail.co.uk>
2016-05-21 14:58:37 +0100

    refactor(derivedData) improve perf

--- src/utils/derivedData.js
+++ src/utils/derivedData.js
@@ -1,29 +1,20 @@
 import {
   compose,
-  curry,
-  equals,
   filter,
   find,
-  lensProp,
   map,
-  view
 } from 'ramda'
 
-const viewEffectInstances = view(lensProp('effectInstances'))
-const viewInstance = view(lensProp('instance'))
-const viewInstrumentInstances = view(lensProp('instrumentInstances'))
-const viewName = view(lensProp('name'))
-
-const getInstance = curry((viewLens, name, plugins) => compose(
-  viewInstance,
-  find(compose(equals(name), viewName)),
-  viewLens
-)(plugins))
-
-export const effectInstance = getInstance(viewEffectInstances)
-export const instrumentInstance = getInstance(viewInstrumentInstances)
+export const effectInstance = (name, plugins) => find(
+  x => name === x.name,
+  plugins.effectInstances
+).instance
+export const instrumentInstance = (name, plugins) => find(
+  x => name === x.name,
+  plugins.instrumentInstances
+).instance
 export const controllableInstrumentInstanceNames = compose(
-  map(viewName),
-  filter(x => viewInstance(x).noteStart),
-  viewInstrumentInstances
+  map(x => x.name),
+  filter(x => x.instance.noteStart),
+  x => x.instrumentInstances
 )
