0454a05a3809415bbea608095751b32c1b94564b
benji6 <benhall22@hotmail.co.uk>
2016-05-21 19:35:50 +0100

    fix(controllableInstrumentInstanceNames)

--- src/utils/derivedData.js
+++ src/utils/derivedData.js
@@ -4,6 +4,7 @@ import {
   find,
   map,
 } from 'ramda'
+import {noop} from './helpers'
 
 export const effectInstance = (name, plugins) => find(
   x => name === x.name,
@@ -15,6 +16,6 @@ export const instrumentInstance = (name, plugins) => find(
 ).instance
 export const controllableInstrumentInstanceNames = compose(
   map(x => x.name),
-  filter(x => x.instance.noteStart),
+  filter(x => x.instance.noteStart !== noop),
   x => x.instrumentInstances
 )
