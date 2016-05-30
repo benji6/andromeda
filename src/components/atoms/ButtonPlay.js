bc6f7c9979b97287864b975179c45127e44aeb35
Ben Hall <benhall22@hotmail.co.uk>
2016-05-30 18:47:03 +0100

    refactor - rename play-button to button-play

--- src/components/atoms/ButtonPlay.js
+++ src/components/atoms/ButtonPlay.js
@@ -4,7 +4,7 @@ import {setPatternPlaying} from '../../actions'
 export default ({onPlay, onStop, dispatch, patternId, playing}) =>
 <div className='text-center'>
   <button
-    className={`play-button${playing ? ' play-button--playing' : ''}`}
+    className={`button-play${playing ? ' button-play--playing' : ''}`}
     onClick={_ => {
       dispatch(setPatternPlaying({patternId, value: !playing}))
       setTimeout(_ => { if (playing) onStop(); else onPlay() })
