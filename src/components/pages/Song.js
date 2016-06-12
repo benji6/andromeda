82751605d2c6874507316b2df9638e5cd202cd5a
Ben Hall <benhall22@hotmail.co.uk>
2016-06-12 11:57:35 +0100

    song play and stop now handled by middleware

--- src/components/pages/Song.js
+++ src/components/pages/Song.js
@@ -1,11 +1,8 @@
-import {forEach, map, reject} from 'ramda'
 import React from 'react'
 import {connect} from 'react-redux'
-import audioContext from '../../audioContext'
 import {
   addNewPattern,
   deletePattern,
-  songActiveNotesSet,
   songPlayingStart,
   songPlayingStop,
 } from '../../actions'
@@ -14,50 +11,42 @@ import ButtonPrimarySmall from '../atoms/ButtonPrimarySmall'
 import ButtonPrimary from '../atoms/ButtonPrimary'
 import {Cross, Plus} from '../atoms/ButtonIcons'
 import {mapIndexed} from '../../utils/helpers'
-import pitchToFrequency from '../../audioHelpers/pitchToFrequency'
-import pitchFromScaleIndex from '../../audioHelpers/pitchFromScaleIndex'
-import scales from '../../constants/scales'
-import patternPitchOffset from '../../constants/patternPitchOffset'
-import {instrumentInstance} from '../../utils/derivedData'
-import {forEachIndexed} from '../../utils/helpers'
 import PatternSvg from '../organisms/PatternSvg'
 
-const connectComponent = connect(({
+const mapStateToProps = ({
   activePatternIndex,
   dispatch,
   instrument,
   patterns,
-  plugins,
-  settings: {bpm, rootNote, selectedScale},
-  song: {activeNotes, playing},
+  song: {playing},
 }) => ({
-  activeNotes,
   activePatternIndex,
-  bpm,
   dispatch,
   instrument,
   patterns,
   playing,
-  plugins,
-  rootNote,
-  selectedScale,
-}))
+})
 
-export default connectComponent(({
-  activeNotes,
-  bpm,
-  dispatch,
+const mapDispatchToProps = {
+  addNewPattern,
+  deletePattern,
+  songPlayingStart,
+  songPlayingStop,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(({
+  addNewPattern,
+  deletePattern,
   patterns,
   playing,
-  plugins,
-  rootNote,
-  selectedScale,
+  songPlayingStart,
+  songPlayingStop,
 }) =>
   <div>
     {patterns.length
       ? mapIndexed((_, i) =>
         <div className='Song__Pattern' key={i}>
-          <Cross onClick={comp(dispatch, deletePattern, K(i))} />
+          <Cross onClick={() => deletePattern(i)} />
           <ButtonPrimarySmall to={`/controllers/pattern/${i}`}>
             {`Pattern ${i}`}
           </ButtonPrimarySmall>
@@ -65,54 +54,11 @@ export default connectComponent(({
         </div>, patterns)
       : null}
     <div className='text-center padding-1'>
-      <Plus onClick={comp(dispatch, addNewPattern)}>New pattern</Plus>
+      <Plus {...{onClick: addNewPattern}}>New pattern</Plus>
     </div>
     <ButtonPlay {...{
-      onPlay: () => {
-        const noteDuration = 60 / bpm
-
-        dispatch(songPlayingStart())
-
-        let newActiveNotes = []
-
-        forEachIndexed(
-          ({instrument, steps, volume, xLength, yLength}, patternId) => {
-            const {currentTime} = audioContext
-            const instrumentObj = instrumentInstance(instrument, plugins)
-
-            newActiveNotes = newActiveNotes.concat(reject(({id}) => {
-                for (const {x, y} of steps) {
-                  if (id.indexOf(`pattern-${patternId}-${x}-${y}`) !== -1) {
-                    return true
-                  }
-                }
-              }, activeNotes).concat(map(({x, y}) => ({
-                id: `song-${patternId}-${x}-${y}`,
-                instrumentObj,
-              }), steps)))
-
-            const notes = map(({x, y}) => ({
-              frequency: pitchToFrequency(pitchFromScaleIndex(
-                scales[selectedScale],
-                yLength - 1 - y
-              ) + rootNote + patternPitchOffset),
-              gain: volume,
-              id: `song-${patternId}-${x}-${y}`,
-              startTime: currentTime + noteDuration * x,
-              stopTime: currentTime + noteDuration * (x + 1),
-            }), steps)
-
-            instrumentObj.notesStart(notes)
-          },
-          patterns
-        )
-
-        dispatch(songActiveNotesSet(newActiveNotes))
-      },
-      onStop: () => {
-        forEach(({id, instrumentObj}) => instrumentObj.noteStop(id), activeNotes)
-        dispatch(songPlayingStop())
-      },
+      onPlay: songPlayingStart,
+      onStop: songPlayingStop,
       playing,
     }}/>
     <nav>
