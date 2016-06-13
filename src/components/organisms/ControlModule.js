4b3a076588dc5794edfdf42435ae5956b315b18a
Ben Hall <benhall22@hotmail.co.uk>
2016-06-13 21:00:56 +0100

    fix ControlModule styles

--- src/components/organisms/ControlModule.js
+++ src/components/organisms/ControlModule.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Input = ({children, label}) =>
-  <label {...{className: 'control-module__Input'}}>
-    <span {...{className: 'control-module__Input__Label'}}>{label}</span>
+  <label {...{className: 'ControlModule__Input'}}>
+    <span {...{className: 'ControlModule__Input__Label'}}>{label}</span>
     {children}
   </label>
 
 export const CheckBox = ({label, ...props}) =>
   <Input {...{label}}>
     <input {...{type: 'checkbox', ...props}} />
-    <div {...{className: 'control-module__Output'}} />
+    <div {...{className: 'ControlModule__Output'}} />
   </Input>
 
 export const Range = ({
@@ -20,26 +20,26 @@ export const Range = ({
   ...props,
 }) => <Input {...{label}}>
   <input {...{
-    className: 'control-module__Range',
+    className: 'ControlModule__Range',
     max,
     min,
     step,
     type: 'range',
     ...props,
   }} />
-  <div {...{className: 'control-module__Output'}}>
+  <div {...{className: 'ControlModule__Output'}}>
     {props.displayValue || props.defaultValue.toFixed(2)}
   </div>
 </Input>
 
 export const Select = ({label, ...props}) => <Input {...{label}}>
-  <select {...{className: 'control-module__Select', ...props}} />
+  <select {...{className: 'ControlModule__Select', ...props}} />
 </Input>
 
 export default ({children, title, ...props}) => <div {...{
-  className: 'control-module',
+  className: 'ControlModule',
   ...props,
 }}>
-  {title ? <h3 {...{className: 'control-module__Title'}}>{title}</h3> : null}
+  {title ? <h3 {...{className: 'ControlModule__Title'}}>{title}</h3> : null}
   <div {...{className: 'display-table'}}>{children}</div>
 </div>
