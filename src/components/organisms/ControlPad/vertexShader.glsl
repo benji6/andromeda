aa7c4a6ddc68da0eb3f948065828387aaa529515
Ben Hall <benhall22@hotmail.co.uk>
2016-07-02 15:58:51 +0100

    remove THREE - use bespoke webgl instead

---
+++ src/components/organisms/ControlPad/vertexShader.glsl
@@ -0,0 +1,11 @@
+attribute vec4 a_position;
+attribute vec4 a_color;
+
+uniform mat4 u_matrix;
+
+varying vec4 v_color;
+
+void main() {
+  gl_Position = u_matrix * a_position;
+  v_color = a_color;
+}
---
+++ src/utils/webGl.js
@@ -0,0 +1,75 @@
+export const compileShader = (gl, src, shaderType) => {
+  const shader = gl.createShader(shaderType)
+  gl.shaderSource(shader, src)
+  gl.compileShader(shader)
+  return shader
+}
+
+export const mult = (a, b) => [
+  a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12],
+  a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13],
+  a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14],
+  a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15],
+  a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12],
+  a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13],
+  a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14],
+  a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15],
+  a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12],
+  a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13],
+  a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14],
+  a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15],
+  a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12],
+  a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13],
+  a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14],
+  a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15],
+]
+
+export const rotateX = a => {
+  const c = Math.cos(a)
+  const s = Math.sin(a)
+
+  return [
+    1, 0, 0, 0,
+    0, c, s, 0,
+    0, -s, c, 0,
+    0, 0, 0, 1
+  ]
+}
+
+export const rotateY = a => {
+  const c = Math.cos(a)
+  const s = Math.sin(a)
+
+  return [
+    c, 0, -s, 0,
+    0, 1, 0, 0,
+    s, 0, c, 0,
+    0, 0, 0, 1
+  ]
+}
+
+export const rotateZ = a => {
+  const c = Math.cos(a)
+  const s = Math.sin(a)
+
+  return [
+     c, s, 0, 0,
+    -s, c, 0, 0,
+     0, 0, 1, 0,
+     0, 0, 0, 1,
+  ]
+}
+
+export const scale = (x, y, z) => [
+  x, 0, 0, 0,
+  0, y, 0, 0,
+  0, 0, z, 0,
+  0, 0, 0, 1,
+]
+
+export const translate = (x, y, z) => [
+   1, 0, 0, 0,
+   0, 1, 0, 0,
+   0, 0, 1, 0,
+   x, y, z, 1
+]
