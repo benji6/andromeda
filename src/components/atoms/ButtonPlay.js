7c2191f23d3f9c5463b7209ae216e6a98a706dda
Ben Hall <benhall22@hotmail.co.uk>
2016-07-08 23:07:19 +0100

    further work on PatternBeat styling

--- src/components/atoms/ButtonPlay.js
+++ src/components/atoms/ButtonPlay.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import {createElement} from 'react'
+import {makeClassName} from '../../utils/helpers'
 
-export default ({onPlay, onStop, playing}) =>
-<div className='text-center'>
-  <button {...{
-    className: `button-play${playing ? ' button-play--playing' : ''}`,
-    onClick: playing ? onStop : onPlay,
-  }} />
-</div>
+export default ({onPlay, onStop, playing}) => createElement('button', {
+  className: makeClassName(['ButtonPlay', [playing, 'ButtonPlay--playing']]),
+  onClick: playing ? onStop : onPlay,
+})
