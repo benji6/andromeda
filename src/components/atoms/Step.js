0b829908fe898242e4e8f16233b6778382be3b67
Ben Hall <benhall22@hotmail.co.uk>
2016-07-10 11:22:35 +0100

    beatPattern is now red

--- src/components/atoms/Step.js
+++ src/components/atoms/Step.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import {createElement} from 'react'
+import {makeClassName} from '../../utils/helpers'
 
-export default ({onClick, selected}) => <button
-  className={`step ${selected === true ? 'step--selected' : ''}`}
-  onClick={onClick}
-/>
+export default ({onClick, red, selected}) => createElement('button', {
+  className: makeClassName([
+    'Step',
+    [red, 'Step--red'],
+    [selected, 'Step--selected'],
+    [red && selected, 'Step--red--selected'],
+  ]),
+  onClick
+})
--- src/components/molecules/PatternRow.js
+++ src/components/molecules/PatternRow.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import {createElement} from 'react'
 import {mapIndexed} from '../../utils/helpers'
 import Step from '../atoms/Step'
 
-export default ({cells, onClick, yLabel}) =>
-  <div className='flex-row pattern-row'>
-    <div className='y-label' key={'flex-row-y-label'}>{yLabel}</div>
-    {mapIndexed((cell, i) => <Step
-      selected={cell.selected}
-      key={`flex-row-${i}`}
-      onClick={onClick(i)}
-    />, cells)}
-  </div>
+export default ({cells, onClick, red, yLabel}) =>
+  createElement('div', {className: 'PatternRow'}, [
+    createElement('div', {className: 'YLabel', key: 'YLabel'}, yLabel),
+    mapIndexed((cell, i) => createElement(Step, {
+      key: i,
+      onClick: onClick(i),
+      red,
+      selected: cell.selected,
+    }), cells)
+  ])
