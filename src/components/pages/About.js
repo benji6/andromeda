de43856277fc3e0bc2f97f30b3cedf4a37ce586e
Ben Hall <benhall22@hotmail.co.uk>
2016-07-28 13:55:05 +0100

    skin:about link hover effects

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default () =>
-  <div className='about'>
-    <h1 className='about__title'>About</h1>
+  <div className='About'>
+    <h1 className='About__Title'>About</h1>
     <p>Elemental is a pluggable digital audio workstation in development built on open web technologies</p>
     <p>All the code is open source and hosted on GitHub&nbsp;
-      <a className='about__link' href='https://github.com/benji6/elemental' target='_blank'>
+      <a className='About__Link' href='https://github.com/benji6/elemental' target='_blank'>
         here
       </a>
     </p>
