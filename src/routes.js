fd5d900bbda24ed426f3d8737ad507c32351c4c5
Ben Hall <benhall22@hotmail.co.uk>
2016-07-30 08:42:29 +0100

    refactor:ControlPadPage

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import About from './components/pages/About'
 import Channel from './components/pages/Channel'
 import Channels from './components/pages/Channels'
 import ControlPadSettings from './components/pages/ControlPadSettings'
-import ControlPad from './components/pages/ControlPad'
+import ControlPadPage from './components/pages/ControlPadPage'
 import Effect from './components/pages/Effect'
 import Instrument from './components/pages/Instrument'
 import KeyboardSettings from './components/pages/KeyboardSettings'
@@ -16,11 +16,11 @@ import Song from './components/pages/Song'
 
 export default <Router history={hashHistory}>
   <Route path='/' component={App}>
-    <IndexRoute component={ControlPad} />
+    <IndexRoute component={ControlPadPage} />
     <Route path='/about' component={About} />
     <Route path='/channel/:channelId' component={Channel} />
     <Route path='/channels' component={Channels} />
-    <Route path='/controllers/control-pad' component={ControlPad} />
+    <Route path='/controllers/control-pad' component={ControlPadPage} />
     <Route path='/controllers/control-pad/settings' component={ControlPadSettings} />
     <Route path='/controllers/keyboard/settings' component={KeyboardSettings} />
     <Route path='/controllers/pattern/:patternId' component={Pattern} />
@@ -29,6 +29,6 @@ export default <Router history={hashHistory}>
     <Route path='/plugins/effects/:name' component={Effect} />
     <Route path='/plugins/instruments/:name' component={Instrument} />
     <Route path='/settings' component={Settings} />
-    <Route path='/*' component={ControlPad} />
+    <Route path='/*' component={ControlPadPage} />
   </Route>
 </Router>
