c96de09be461585f0eec25d285593a5e367522e5
Ben Hall <benhall22@hotmail.co.uk>
2016-07-30 08:22:19 +0100

    feat:About add link to GitHub issues

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import LinkExternal from '../atoms/LinkExternal'
 
 export default () =>
   <div className='About'>
     <h1 className='About__Title'>About</h1>
     <p>Elemental is a pluggable digital audio workstation in development built on open web technologies</p>
-    <p>All the code is open source and hosted on GitHub&nbsp;
-      <a className='About__Link' href='https://github.com/benji6/elemental' target='_blank'>
-        here
-      </a>
+    <p>
+      All the code is open source and hosted on&nbsp;
+      <LinkExternal href='https://github.com/benji6/elemental'>
+        GitHub
+      </LinkExternal>
     </p>
-    <p>Contributions, issues, feedback and suggestions are all very welcome!</p>
+    <p>
+      If you would like to report a bug or request a feature raise an&nbsp;
+      <LinkExternal href='https://github.com/benji6/elemental/issues'>
+        issue
+      </LinkExternal>
+    </p>
+    <p>
+      Contributions, issues, feedback and suggestions are all very welcome!
+    </p>
   </div>
