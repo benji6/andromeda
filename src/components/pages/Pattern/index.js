fdbea3c3a03ea1e6391309487c3efd1be7bfe652
Ben Hall <benhall22@hotmail.co.uk>
2016-07-31 12:02:22 +0100

    refactor:Pattern remove loads of pointless code

--- src/components/pages/Pattern/index.js
+++ src/components/pages/Pattern/index.js
@@ -1,63 +1,11 @@
-import {
-  map,
-  range,
-  repeat,
-} from 'ramda'
 import {createElement} from 'react'
 import {connect} from 'react-redux'
-import {defaultMemoize} from 'reselect'
-import {mapIndexed} from '../../../utils/helpers'
-import {stepExists} from '../../../reducers/patterns'
 import PatternBeat from './PatternBeat'
 import PatternSynth from './PatternSynth'
 
-const emptyPatternData = defaultMemoize((xLength, yLength) =>
-  map(range(0), repeat(xLength, yLength)))
-
-const mapStateToProps = ({
-  dispatch,
-  patterns,
-  plugins,
-  settings: {bpm, rootNote, selectedScale},
-}, {params: {patternId}}) => {
-  const {
-    beatPattern,
-    instrument,
-    markerPosition,
-    playing,
-    playStartTime,
-    steps,
-    volume,
-    xLength,
-    yLength,
-  } = patterns[patternId]
-
-  const patternData = mapIndexed(
-    (x, rowIndex) => map(colIndex => ({
-      selected: stepExists(colIndex, rowIndex, steps),
-    }), x),
-    emptyPatternData(xLength, yLength)
-  )
-
-  return {
-    beatPattern,
-    bpm,
-    dispatch,
-    instrument,
-    markerPosition,
-    patternData,
-    patternId: Number(patternId),
-    playing,
-    playStartTime,
-    plugins,
-    rootNote,
-    selectedScale,
-    steps,
-    volume,
-    xLength,
-    yLength,
-  }
-}
+const mapStateToProps = ({patterns}, {params: {patternId}}) => ({
+  beatPattern: patterns[patternId].beatPattern,
+})
 
 export default connect(mapStateToProps)(
   props => props.beatPattern
