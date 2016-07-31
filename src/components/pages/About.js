be274ff865d974ed6d410374807685feef631af1
Ben Hall <benhall22@hotmail.co.uk>
2016-07-31 21:05:43 +0100

    refactor:About no more jsx

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import {createElement} from 'react'
 import LinkExternal from '../atoms/LinkExternal'
 
 export default () =>
-  <div className='About'>
-    <h1 className='About__Title'>About</h1>
-    <p>Elemental is a pluggable digital audio workstation in development built on open web technologies</p>
-    <p>
-      All the code is open source and hosted on&nbsp;
-      <LinkExternal href='https://github.com/benji6/elemental'>
-        GitHub
-      </LinkExternal>
-    </p>
-    <p>
-      If you would like to report a bug or request a feature raise an&nbsp;
-      <LinkExternal href='https://github.com/benji6/elemental/issues'>
-        issue
-      </LinkExternal>
-    </p>
-    <p>
-      Contributions, issues, feedback and suggestions are all very welcome!
-    </p>
-  </div>
+  createElement('div', {className: 'About'},
+    createElement('h1', {className: 'About__Title'}, 'About'),
+    createElement('p', null, 'Elemental is a pluggable digital audio workstation in development built on open web technologies'),
+    createElement('p', null,
+      'All the code is open source and hosted on ',
+      createElement(
+        LinkExternal,
+        {href: 'https://github.com/benji6/elemental'},
+        'GitHub'
+      )
+    ),
+    createElement('p', null,
+      'If you would like to report a bug or request a feature raise an ',
+      createElement(
+        LinkExternal,
+        {href: 'https://github.com/benji6/elemental/issues'},
+        'issue'
+      )
+    ),
+    createElement('p', null,
+      'Contributions, issues, feedback and suggestions are all very welcome!'
+    )
+  )
