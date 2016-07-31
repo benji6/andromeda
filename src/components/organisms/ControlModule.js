d32d6f21b6944794757576115a7b7011975c26f0
Ben Hall <benhall22@hotmail.co.uk>
2016-07-31 10:36:49 +0100

    refactor:organisms remove jsx

--- src/components/organisms/ControlModule.js
+++ src/components/organisms/ControlModule.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import {createElement} from 'react'
 
 const Input = ({children, label}) =>
-  <label {...{className: 'ControlModule__Input'}}>
-    <span {...{className: 'ControlModule__Input__Label'}}>{label}</span>
-    {children}
-  </label>
+  createElement('label', {className: 'ControlModule__Input'},
+    createElement('span', {className: 'ControlModule__Input__Label'}, label),
+    children
+  )
 
 export const CheckBox = ({label, ...props}) =>
-  <Input {...{label}}>
-    <input {...{type: 'checkbox', ...props}} />
-    <div {...{className: 'ControlModule__Output'}} />
-  </Input>
-
+  createElement(Input, {label},
+    createElement('input', {type: 'checkbox', ...props},
+      createElement('div', {className: 'ControlModule__Output'})
+    )
+  )
 export const Range = ({
   label,
   max = 1,
   min = 0,
   step = (max - min) / 1000,
   ...props,
-}) => <Input {...{label}}>
-  <input {...{
-    className: 'ControlModule__Range',
-    max,
-    min,
-    step,
-    type: 'range',
-    ...props,
-  }} />
-  <div {...{className: 'ControlModule__Output'}}>
-    {props.displayValue || props.defaultValue.toFixed(2)}
-  </div>
-</Input>
+}) =>
+  createElement(Input, {label},
+    createElement('input', {
+      className: 'ControlModule__Range',
+      max,
+      min,
+      step,
+      type: 'range',
+      ...props,
+    }),
+    createElement('div', {className: 'ControlModule__Output'},
+      props.displayValue || props.defaultValue.toFixed(2)
+    )
+  )
 
-export const Select = ({label, ...props}) => <Input {...{label}}>
-  <select {...{className: 'ControlModule__Select', ...props}} />
-</Input>
+export const Select = ({label, ...props}) =>
+  createElement(Input, {label},
+    createElement('select', {className: 'ControlModule__Select', ...props})
+)
 
-export default ({children, title, ...props}) => <div {...{
-  className: 'ControlModule',
-  ...props,
-}}>
-  {title ? <h3 {...{className: 'ControlModule__Title'}}>{title}</h3> : null}
-  <div {...{className: 'display-table'}}>{children}</div>
-</div>
+export default ({children, title, ...props}) =>
+  createElement('div', {className: 'ControlModule', ...props},
+  title && createElement('h3', {className: 'ControlModule__Title'}, title),
+  createElement('div', {className: 'display-table'}, children)
+)
--- src/components/organisms/Pattern.js
+++ src/components/organisms/Pattern.js
@@ -1,11 +1,11 @@
 import {range} from 'ramda'
+import {Component, createElement} from 'react'
 import {mapIndexed} from '../../utils/helpers'
-import React from 'react'
 import XLabels from '../molecules/XLabels'
 import PatternRow from '../molecules/PatternRow'
 import {makeClassName} from '../../utils/helpers'
 
-export default class extends React.Component {
+export default class extends Component {
   componentDidMount () {
     const patternBody = this.refs.pattern__body
     patternBody.scrollTop = patternBody.clientHeight
@@ -13,26 +13,26 @@ export default class extends React.Component {
   render () {
     const {markerPosition, onClick, patternData, red, yLabel} = this.props
     const markerLeft = 100 / (patternData[0].length + 1)
-    return <div className='pattern'>
-      <div {...{
+    return createElement('div', {className: 'pattern'},
+      createElement('div', {
         className: makeClassName(['pattern__marker', [red, 'pattern__marker--pink']]),
         style: {
           transform: `translateX(${markerLeft + (100 - markerLeft - 0.9) * markerPosition - 0.3}vw)`,
         },
-      }}/>
-      <XLabels labels={range(0, patternData[0].length + 1)} />
-      <div {...{className: 'pattern__body', ref: 'pattern__body'}}>
-        {mapIndexed(
-          (rowData, i) => <PatternRow {...{
+      }),
+      createElement(XLabels, {labels: range(0, patternData[0].length + 1)}),
+      createElement('div', {className: 'pattern__body', ref: 'pattern__body'},
+        mapIndexed(
+          (rowData, i) => createElement(PatternRow, {
             cells: rowData,
             key: `PatternRow-${i}`,
             onClick: onClick(i),
             red,
             yLabel: yLabel(i),
-          }}/>,
+          }),
           patternData
-        )}
-      </div>
-    </div>
+        )
+      )
+    )
   }
 }
