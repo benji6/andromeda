5eb9289fdefb96d8c25d790e6a85e808d26a178d
Ben Hall <benhall22@hotmail.co.uk>
2016-08-21 14:58:45 +0100

    refactor:KeyboardSettings remove jsx

--- src/components/pages/KeyboardSettings.js
+++ src/components/pages/KeyboardSettings.js
@@ -1,6 +1,6 @@
 import capitalize from 'capitalize'
 import {map} from 'ramda'
-import React from 'react'
+import {createElement} from 'react'
 import {connect} from 'react-redux'
 import {controllableInstrumentInstanceNames} from '../../utils/derivedData'
 import {eventValuePath} from '../../utils/helpers'
@@ -16,49 +16,48 @@ import InputLabel from '../atoms/InputLabel'
 import CheckboxSelector from '../molecules/CheckboxSelector'
 import RangeSelector from '../molecules/RangeSelector'
 
-const connectComponent = connect(({keyboard, dispatch, plugins}) =>
-  ({keyboard, dispatch, plugins}))
+const mapStateToProps = ({keyboard, dispatch, plugins}) => ({keyboard, dispatch, plugins})
 
-export default connectComponent(({keyboard, dispatch, plugins}) =>
-  <div className='KeyboardSettings'>
-    <h2>Keyboard Settings</h2>
-    <InstrumentSelector
-      defaultValue={keyboard.instrument}
-      handleChange={(comp(
+export default connect(mapStateToProps)(({keyboard, dispatch, plugins}) =>
+  createElement('div', {className: 'KeyboardSettings'},
+    createElement('h2', null, 'Keyboard Settings'),
+    createElement(InstrumentSelector, {
+      defaultValue: keyboard.instrument,
+      handleChange: comp(
         dispatch,
         keyboardInstrumentSet,
         eventValuePath
-      ))}
-      label='Instrument'
-      options={map(
+      ),
+      label: 'Instrument',
+      options: map(
         instrument => ({text: capitalize.words(instrument), value: instrument}),
         controllableInstrumentInstanceNames(plugins)
-      )}
-    />
-    <RangeSelector
-      max='1'
-      min='0'
-      step='0.01'
-      onChange={comp(dispatch, keyboardVolumeSet, Number, eventValuePath)}
-      output={Math.round(keyboard.volume * 100)}
-      text='Volume'
-      value={keyboard.volume}
-    />
-    <RangeSelector
-      max='2'
-      min='-3'
-      onChange={comp(dispatch, keyboardOctaveSet, Number, eventValuePath)}
-      output={keyboard.octave}
-      text='Octave'
-      value={keyboard.octave}
-    />
-    <CheckboxSelector {...{
+      ),
+    }),
+    createElement(RangeSelector, {
+      max: 1,
+      min: 0,
+      onChange: comp(dispatch, keyboardVolumeSet, Number, eventValuePath),
+      output: Math.round(keyboard.volume * 100),
+      step: 0.01,
+      text: 'Volume',
+      value: keyboard.volume,
+    }),
+    createElement(RangeSelector, {
+      max: 2,
+      min: -3,
+      onChange: comp(dispatch, keyboardOctaveSet, Number, eventValuePath),
+      output: keyboard.octave,
+      text: 'Octave',
+      value: keyboard.octave,
+    }),
+    createElement(CheckboxSelector, {
       checked: keyboard.monophonic,
       onChange: e => dispatch(keyboardMonophonicSet(e.target.checked)),
       text: 'Monophonic',
-    }} />
-    <div>
-      <InputLabel />
-      <ButtonPrimary to='/settings'>OK</ButtonPrimary>
-    </div>
-  </div>)
+    }),
+    createElement('div', null,
+      createElement(InputLabel),
+      createElement(ButtonPrimary, {to: '/settings'}, 'OK')
+    )
+  ))
