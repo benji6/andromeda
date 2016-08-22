0138f758392609d139790c09a26a3fd4bf411d77
Ben Hall <benhall22@hotmail.co.uk>
2016-08-22 20:39:52 +0100

    refactor:Prometheus no more jsx

--- src/plugins/instruments/Prometheus/components/ModuleLfo.js
+++ src/plugins/instruments/Prometheus/components/ModuleLfo.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import {createElement} from 'react'
 import ControlModule, {Range} from '../../../../components/organisms/ControlModule'
 import SelectOscType from './SelectOscType'
 
 export default ({lfo: {gain, frequency, type}, updateLfo}) =>
-  <ControlModule {...{title: 'LFO'}}>
-    <SelectOscType {...{
+  createElement(ControlModule, {title: 'LFO'},
+    createElement(SelectOscType, {
       defaultValue: type,
       onChange: e => updateLfo('type', e.target.value),
-    }} />
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: frequency,
       label: 'Rate',
       max: 16,
       min: 0.01,
       onInput: e => updateLfo('frequency', Number(e.target.value)),
-    }} />
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: gain,
       displayValue: Math.round(gain),
       label: 'Amount',
       max: 1024,
       onInput: e => updateLfo('gain', Number(e.target.value)),
-    }} />
-  </ControlModule>
+    })
+  )
--- src/plugins/instruments/Prometheus/components/ModuleOsc.js
+++ src/plugins/instruments/Prometheus/components/ModuleOsc.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import {createElement} from 'react'
 import ControlModule, {Range} from '../../../../components/organisms/ControlModule'
 import SelectOscType from './SelectOscType'
 
 export default ({i, updateOsc, settings}) =>
-  <ControlModule {...{title: `Osc ${i + 1}`}} >
-    <SelectOscType {...{
+  createElement(ControlModule, {title: `Osc ${i + 1}`},
+    createElement(SelectOscType, {
       defaultValue: settings.type,
       onChange: ({target: {value}}) => updateOsc('type', value),
-    }}/>
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: settings.gain,
       label: 'Gain',
       max: 2,
       onInput: e => updateOsc('gain', Number(e.target.value)),
-    }} />
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: settings.pan,
       label: 'Pan',
       min: -1,
       onInput: e => updateOsc('pan', Number(e.target.value)),
-    }} />
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: settings.pitch,
       displayValue: settings.pitch,
       label: 'Pitch',
@@ -28,13 +28,13 @@ export default ({i, updateOsc, settings}) =>
       min: -24,
       onInput: e => updateOsc('pitch', Number(e.target.value)),
       step: 1,
-    }} />
-    <Range {...{
+    }),
+    createElement(Range, {
       defaultValue: settings.detune,
       displayValue: settings.detune,
       label: 'Detune',
       max: 50,
       min: -50,
       onInput: e => updateOsc('detune', Number(e.target.value)),
-    }} />
-  </ControlModule>
+    })
+  )
