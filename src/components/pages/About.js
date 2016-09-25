e6cc3962a50ef89f401b9c4123857dd4a11c5d8e
Ben Hall <benhall22@hotmail.co.uk>
2016-09-25 19:15:45 +0100

    About: add instructions for adding app to home screen

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,6 +12,8 @@ export default connect(mapStateToProps)(({lastDirection}) =>
       lastDirection === 'left' ? 'slide-in-left' : 'slide-in-right'
     ),
   },
+    createElement('h1', {className: 'About__Title'}, 'Tips'),
+    createElement('p', null, 'For best results use Chrome and on mobile the app is much nicer when added to the home screen and launched from there (tap "Add to Home Screen" in the browser menu)'),
     createElement('h1', {className: 'About__Title'}, 'About'),
     createElement('p', null, 'Elemental is a pluggable digital audio workstation in development built on open web technologies'),
     createElement('p', null,
@@ -23,14 +25,14 @@ export default connect(mapStateToProps)(({lastDirection}) =>
       )
     ),
     createElement('p', null,
-      'If you would like to report a bug or request a feature raise an ',
+      'If you would like to report a bug or request a feature you can raise an issue ',
       createElement(
         LinkExternal,
         {href: 'https://github.com/benji6/elemental/issues'},
-        'issue'
+        'here'
       )
     ),
     createElement('p', null,
-      'Contributions, issues, feedback and suggestions are all very welcome!'
+      'Contributions, feedback and suggestions are all very welcome!'
     )
   ))
