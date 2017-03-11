da631531910df07e450f4bc89dcea832cdc01df7
Ben Hall <benji2357@gmail.com>
2017-03-11 19:56:27 +0000

    feat:Settings user can no longer set color

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,7 +1,6 @@
 import {assoc, merge} from 'ramda'
 import {
   BPM_SET,
-  ROOT_HUE_SET,
   ROOT_NOTE_SET,
   SELECTED_SCALE_SET,
 } from '../actions'
@@ -11,7 +10,6 @@ const bpm = 140
 const initialState = {
   bpm,
   noteDuration: 60 / bpm,
-  rootHue: 0,
   rootNote: 0,
   selectedScale: 'pentatonic',
 }
@@ -19,7 +17,6 @@ const initialState = {
 export default (state = initialState, {type, payload}) => {
   switch (type) {
     case BPM_SET: return merge(state, {bpm: payload, noteDuration: 60 / payload})
-    case ROOT_HUE_SET: return assoc('rootHue', payload, state)
     case ROOT_NOTE_SET: return assoc('rootNote', payload, state)
     case SELECTED_SCALE_SET: return assoc('selectedScale', payload, state)
     default: return state
