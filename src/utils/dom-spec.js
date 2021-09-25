74a2ccf0b4613109d298cd948a0af8a7a95c0987
Ben Hall <benji2357@gmail.com>
2021-09-25 16:45:32 +0100

    test(*): remove all tests

--- src/utils/dom-spec.js
+++
@@ -1,8 +0,0 @@
-import test from "tape";
-import { makeClassName } from "./dom";
-
-test("helpers - makeClassName", (t) => {
-  t.equal(makeClassName("a", "b", "c"), "a b c");
-  t.equal(makeClassName("a", false && "b", "c"), "a c");
-  t.end();
-});
