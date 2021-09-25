a3b6e5b337d3ab0df910e3da1681a861fe7e5e5c
Ben Hall <benji2357@gmail.com>
2021-09-25 17:12:15 +0100

    chore(build): migrate to parcel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+import "./vars/colors.css";
+import "./constants/timings.css";
+import "./utils/keyframes.css";
+import "./utils/globals.css";
+
+import "./components/atoms/ButtonIcon.css";
+import "./components/atoms/ButtonPrimary.css";
+import "./components/atoms/ButtonSecondary.css";
+import "./components/atoms/HollowButton.css";
+import "./components/atoms/InputLabel.css";
+import "./components/atoms/InputSelect.css";
+import "./components/atoms/LinkExternal.css";
+import "./components/atoms/WelcomeMessage.css";
+import "./components/molecules/CheckboxLabelled.css";
+import "./components/molecules/RangeLabelled.css";
+import "./components/organisms/ControlModule.css";
+import "./components/organisms/ControlPad/style.css";
+import "./components/organisms/Navigation.css";
+import "./components/organisms/spinner.css";
+import "./components/organisms/table-center.css";
+import "./components/pages/About/style.css";
+import "./components/pages/Channel.css";
+import "./components/pages/ControlPadPage.css";
+import "./components/pages/ControlPadSettings.css";
+import "./components/pages/Effect.css";
+import "./components/pages/Instrument.css";
+import "./components/pages/KeyboardSettings.css";
+import "./components/pages/Settings/style.css";
+
 import { createElement } from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
