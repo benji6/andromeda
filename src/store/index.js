df202dca67d469535b6a5d323266d7b0f0588e32
Ben Hall <benji2357@gmail.com>
2021-09-26 19:56:45 +0100

    refactor(store): remove functionality for persisting state to storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,5 @@
-import { pick } from "ramda";
-import localForage from "localforage";
 import { createStore } from "redux";
-import { autoRehydrate, createTransform, persistStore } from "redux-persist";
 import reducer from "./reducer";
 import enhancer from "./enhancer";
 
-const store = createStore(reducer, enhancer, autoRehydrate());
-
-export default store;
-const saveFilterTransform = (whitelist, keys) =>
-  createTransform(pick(keys), null, { whitelist });
-
-export const rehydratePromise = new Promise((resolve, reject) =>
-  persistStore(
-    store,
-    {
-      debounce: 100,
-      storage: localForage,
-      transforms: [
-        saveFilterTransform("controlPad", [
-          "instrument",
-          "noScale",
-          "octave",
-          "portamento",
-          "range",
-        ]),
-      ],
-      whitelist: [],
-    },
-    (err, state) => {
-      if (err) reject(err);
-      else resolve(state);
-    }
-  )
-);
+export default createStore(reducer, enhancer);
--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,6 +1,4 @@
 import { applyMiddleware } from "redux";
-import createActionBuffer from "redux-action-buffer";
-import { REHYDRATE } from "redux-persist/constants";
 import bpm from "../middlewareRedux/bpm";
 
-export default applyMiddleware(createActionBuffer(REHYDRATE), bpm);
+export default applyMiddleware(bpm);
