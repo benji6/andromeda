c56493627cff3b7b557f77346ed297df95236f43
Ben Hall <benji2357@gmail.com>
2025-02-24 21:59:58 +0000

    refactor(store): migrate from JS to TS

--- src/store/enhancer.js
+++
@@ -1,9 +0,0 @@
-import { compose } from "redux";
-import middleware from "./middleware";
-
-export default compose(
-  middleware,
-  process.env.NODE_ENV !== "production" && window.devToolsExtension
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : (x) => x,
-);
