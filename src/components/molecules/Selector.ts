9befc0a7c0faada62d2fb45bc8cacc7ad1fd2c14
Ben Hall <benji2357@gmail.com>
2025-02-24 22:24:09 +0000

    refactor(molecules): migrate from JS to TS

--- src/components/molecules/Selector.ts
+++ src/components/molecules/Selector.ts
@@ -3,8 +3,22 @@ import { capitalizeFirst } from "../../utils/helpers";
 import InputLabel from "../atoms/InputLabel";
 import InputSelect from "../atoms/InputSelect";
 
-export default ({ defaultValue, disabled, handleChange, label, options }) =>
-  createElement(
+interface Props {
+  defaultValue: string;
+  disabled: boolean;
+  handleChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  label: string;
+  options: { text: string; value: string }[];
+}
+
+export default function Selector({
+  defaultValue,
+  disabled,
+  handleChange,
+  label,
+  options,
+}: Props) {
+  return createElement(
     "label",
     { className: "Selector" },
     createElement(InputLabel, null, capitalizeFirst(label)),
@@ -15,3 +29,4 @@ export default ({ defaultValue, disabled, handleChange, label, options }) =>
       value: defaultValue,
     }),
   );
+}
