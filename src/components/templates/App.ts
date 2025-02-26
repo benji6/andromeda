340e47e9ac138bf01738acc11703125524d82ae8
Ben Hall <benji2357@gmail.com>
2025-02-26 21:51:03 +0000

    refactor(templates): migrate to TSX

--- src/components/templates/App.ts
+++
@@ -1,5 +0,0 @@
-import { createElement } from "react";
-import Navigation from "../organisms/Navigation";
-
-export default ({ children }) =>
-  createElement("div", null, createElement(Navigation), children);
