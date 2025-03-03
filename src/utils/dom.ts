17b83cc11ef8ce2924a3d51492035574075fd921
Ben Hall <benji2357@gmail.com>
2025-03-03 20:34:29 +0000

    refactor(dom): add TypeScript types to eventRatiosAndCoords function

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,6 +1,8 @@
 const validRatio = (n: number) => Math.max(0, Math.min(1 - Number.EPSILON, n));
 
-export const eventRatiosAndCoords = (e) => {
+export const eventRatiosAndCoords = (
+  e,
+): { x: number; xRatio: number; y: number; yRatio: number } => {
   const { top, right, bottom, left } = e.target.getBoundingClientRect();
   const [width, height] = [right - left, bottom - top];
   const { clientX, clientY } = (e.changedTouches && e.changedTouches[0]) || e;
